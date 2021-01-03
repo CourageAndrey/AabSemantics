95f3d913991089ec6655e4ef97347b6ea3a25846
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-01-03 10:10:53 +0300

    Move code of application and exception handling to Inventor.Client project.

--- Code/Inventor.Client/InventorApplication.cs
+++ Code/Inventor.Client/InventorApplication.cs
@@ -1,21 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Threading;
 
 using Inventor.Client.UI;
 using Inventor.Core;
 using Inventor.Core.Localization;
 
-using Sef.Program;
+using Sef.Localization;
+using Sef.Xml;
 
 namespace Inventor.Client
 {
-	public class InventorApplication : ExtendedApplication<InventorConfiguration, LanguageEx, MainWindow>
+	public class InventorApplication : Application
 	{
 		#region Properties
 
-		public KnowledgeBase KnowledgeBase
-		{ get; internal set; }
+		public String StartupPath
+		{ get { return AppDomain.CurrentDomain.BaseDirectory; } }
 
-		public override LanguageEx CurrentLanguage
+		public LanguageEx CurrentLanguage
 		{
 			get { return LanguageEx.CurrentEx; }
 			set
@@ -25,13 +32,50 @@ namespace Inventor.Client
 			}
 		}
 
+		public IList<LanguageEx> Languages
+		{ get; }
+
+		public InventorConfiguration Configuration
+		{ get; }
+
+		public MainWindow MainForm
+		{ get; }
+
+		private String ConfigurationFile
+		{ get { return Path.Combine(StartupPath, InventorConfiguration.FileName); } }
+
+		public KnowledgeBase KnowledgeBase
+		{ get; internal set; }
+
 		#endregion
 
 		public InventorApplication()
-			: base(AppDomain.CurrentDomain)
 		{
+			// exception handling
+			DispatcherUnhandledException += dispatcherUnhandledException;
+			AppDomain.CurrentDomain.UnhandledException += dispatcherAppDomainException;
+
+			// addition languages
+			var languages = Language.LoadAdditional<LanguageEx>(StartupPath);
+			languages.Add(CurrentLanguage);
+			Languages = languages.AsReadOnly();
+
+			// configuration
+			try
+			{
+				Configuration = ConfigurationFile.DeserializeFromFile<InventorConfiguration>();
+			}
+			catch
+			{
+				Configuration = new InventorConfiguration();
+				SaveConfiguration();
+			}
+
+			// form
+			MainForm = new MainWindow();
+
 			MainWindow = MainForm;
-			ShutdownMode = System.Windows.ShutdownMode.OnMainWindowClose;
+			ShutdownMode = ShutdownMode.OnMainWindowClose;
 		}
 
 		[STAThread]
@@ -48,6 +92,56 @@ namespace Inventor.Client
 			application.MainForm.Initialize(application);
 			application.MainWindow.Show();
 			application.Run();
+		}
+
+		#region Exception handling
+
+		private static void dispatcherAppDomainException(object sender, UnhandledExceptionEventArgs e)
+		{
+			if (e.ExceptionObject is Exception)
+			{
+				new ExceptionDialog(new ExceptionWrapper(e.ExceptionObject as Exception), ExceptionDialog.Mode.ProcessFatalError).ShowDialog();
+			}
+		}
+
+		private void dispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			if (new ExceptionDialog(e.Exception, false).ShowDialog() == true)
+			{
+				Shutdown();
+			}
+			e.Handled = true;
+		}
+
+		#endregion
+
+		protected override void OnLoadCompleted(NavigationEventArgs e)
+		{
+			base.OnLoadCompleted(e);
+
+			// select language
+			if (!String.IsNullOrEmpty(Configuration.SelectedLanguage))
+			{
+				var storedLanguage = Languages.FirstOrDefault(l => l.Culture == Configuration.SelectedLanguage);
+				if (storedLanguage != null)
+				{
+					CurrentLanguage = storedLanguage;
+				}
+				else
+				{
+					Configuration.SelectedLanguage = CurrentLanguage.Culture;
+				}
+			}
+			else
+			{
+				CurrentLanguage = Language.FindAppropriate(Languages, CurrentLanguage);
+				Configuration.SelectedLanguage = CurrentLanguage.Culture;
+			}
+		}
+
+		public void SaveConfiguration()
+		{
+			Configuration.SerializeToFile(ConfigurationFile);
 		}
 	}
 }
---
+++ Code/Inventor.Client/UI/ExceptionDialog.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+using Microsoft.Win32;
+
+using Sef.Localization;
+using Sef.Xml;
+
+namespace Inventor.Client.UI
+{
+	public partial class ExceptionDialog : ILocalizable
+	{
+		public enum Mode
+		{
+			ViewOnly,
+			ShowInnerExeption,
+			ProcessError,
+			ProcessFatalError
+		}
+
+		private readonly Mode _mode;
+		private readonly IExceptionWrapper _exception;
+
+		#region Constructor
+
+		public ExceptionDialog(IExceptionWrapper exception, Mode mode)
+		{
+			_mode = mode;
+
+			InitializeComponent();
+
+			SetBinding(TitleProperty, new Binding("Errors.DialogHeader")
+			{
+				Source = Resources["language"],
+				Mode = BindingMode.OneTime
+			});
+
+			buttonInnerException.Visibility = (exception.InnerException == null) ? Visibility.Collapsed : Visibility.Visible;
+			switch (mode)
+			{
+				case Mode.ViewOnly:
+				case Mode.ShowInnerExeption:
+				case Mode.ProcessFatalError:
+					buttonAbort.Visibility = Visibility.Collapsed;
+					buttonIgnore.Visibility = Visibility.Collapsed;
+					break;
+				case Mode.ProcessError:
+					buttonClose.Visibility = Visibility.Collapsed;
+					break;
+				default:
+					throw new NotSupportedException();
+			}
+			Localize();
+			gridData.DataContext = _exception = exception;
+		}
+
+		public ExceptionDialog(IExceptionWrapper exception, Boolean viewOnly)
+			: this(exception, viewOnly ? Mode.ViewOnly : Mode.ProcessError)
+		{ }
+
+		public ExceptionDialog(Exception exception, Boolean viewOnly)
+			: this(new ExceptionWrapper(exception), viewOnly)
+		{ }
+
+		#endregion
+
+		#region Implementation of ILocalizable
+
+		public void Localize()
+		{
+			((ObjectDataProvider) Resources["language"]).Refresh();
+			switch (_mode)
+			{
+				case Mode.ViewOnly:
+					labelCommonMessage.Text = Sef.Localization.Language.Current.Errors.DialogMessageView;
+					break;
+				case Mode.ShowInnerExeption:
+					labelCommonMessage.Text = Sef.Localization.Language.Current.Errors.DialogMessageInner;
+					break;
+				case Mode.ProcessError:
+					labelCommonMessage.Text = Sef.Localization.Language.Current.Errors.DialogMessageCommon;
+					break;
+				case Mode.ProcessFatalError:
+					labelCommonMessage.Text = Sef.Localization.Language.Current.Errors.DialogMessageFatal;
+					break;
+				default:
+					throw new NotSupportedException();
+			}
+			saveDialog.Title = Sef.Localization.Language.Current.Common.SaveFile;
+			saveDialog.Filter = Sef.Localization.Language.Current.Errors.SaveFilter;
+		}
+
+		#endregion
+
+		#region Button handlers
+
+		private void buttonAbort_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+
+		private readonly SaveFileDialog saveDialog = new SaveFileDialog
+		{
+			RestoreDirectory = true
+		};
+
+		private void buttonSave_Click(Object sender, RoutedEventArgs e)
+		{
+			if (saveDialog.ShowDialog() == true)
+			{
+				_exception.SerializeToFile(saveDialog.FileName);
+			}
+		}
+
+		private void buttonInnerException_Click(Object sender, RoutedEventArgs e)
+		{
+			new ExceptionDialog(_exception.InnerException, Mode.ShowInnerExeption).ShowDialog();
+		}
+
+		#endregion
+	}
+}
