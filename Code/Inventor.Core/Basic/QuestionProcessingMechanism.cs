acf8f44bc74fa18b3465440612d6dca702ea28f1
Andrei Bychko <bublic-xiii@list.ru>
2021-01-08 22:04:04 +0300

    Extract QuestionProcessingMechanism class, which allows support of custom question classes.

---
+++ Code/Inventor.Core/Basic/QuestionProcessingMechanism.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Inventor.Core.Localization;
+
+namespace Inventor.Core
+{
+	public class QuestionProcessingMechanism
+	{
+		public ICollection<Type> SupportedQuestionTypes
+		{ get { return _allProcessors.Keys; } }
+
+		private readonly IDictionary<Type, Func<QuestionProcessor>> _allProcessors = new Dictionary<Type, Func<QuestionProcessor>>();
+
+		public QuestionProcessingMechanism()
+		{
+			foreach (var processorType in Assembly.GetExecutingAssembly().GetTypes().Where(t => typeof(QuestionProcessor).IsAssignableFrom(t) && !t.IsAbstract))
+			{
+				var type = processorType;
+				while (!type.IsGenericType || type.GetGenericTypeDefinition() != typeof(QuestionProcessor<>))
+				{
+					type = type.BaseType;
+				}
+				RegisterQuestionProcessor(
+					type.GetGenericArguments()[0],
+					() => Activator.CreateInstance(processorType) as QuestionProcessor);
+			}
+		}
+
+		public void RegisterQuestionProcessor(Type questionType, Func<QuestionProcessor> processorFactory)
+		{
+			_allProcessors[questionType] = processorFactory;
+		}
+
+		public FormattedText Process(KnowledgeBase knowledgeBase, Question question, ILanguage language)
+		{
+			Func<QuestionProcessor> processorFactory;
+			if (_allProcessors.TryGetValue(question.GetType(), out processorFactory))
+			{
+				var processor = processorFactory();
+				return processor.Process(knowledgeBase, question, language);
+			}
+			else
+			{
+				throw new KeyNotFoundException(language.ErrorsInventor.UnknownQuestion);
+			}
+		}
+	}
+}
