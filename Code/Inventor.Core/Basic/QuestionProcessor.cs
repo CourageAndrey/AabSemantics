3a22c338b7144c9c100d3797ebcc16c3e2c710f2
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-01-08 22:04:04 +0300

    Extract QuestionProcessingMechanism class, which allows support of custom question classes.

--- Code/Inventor.Core/Basic/QuestionProcessor.cs
+++ Code/Inventor.Core/Basic/QuestionProcessor.cs
@@ -1,43 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-
 using Inventor.Core.Localization;
 
 namespace Inventor.Core
 {
 	public abstract class QuestionProcessor
 	{
-		public static FormattedText Process(KnowledgeBase knowledgeBase, Question question, ILanguage language)
-		{
-			QuestionProcessor processor;
-			if (_allProcessors.TryGetValue(question.GetType(), out processor))
-			{
-				return processor.ProcessInternal(knowledgeBase, question, language);
-			}
-			else
-			{
-				throw new KeyNotFoundException(language.ErrorsInventor.UnknownQuestion);
-			}
-		}
-
-		private static readonly IDictionary<Type, QuestionProcessor> _allProcessors = new Dictionary<Type, QuestionProcessor>();
-
-		static QuestionProcessor()
-		{
-			foreach (var processorType in Assembly.GetExecutingAssembly().GetTypes().Where(t => typeof (QuestionProcessor).IsAssignableFrom(t) && !t.IsAbstract))
-			{
-				var type = processorType;
-				while (!type.IsGenericType || type.GetGenericTypeDefinition() != typeof (QuestionProcessor<>))
-				{
-					type = type.BaseType;
-				}
-				_allProcessors[type.GetGenericArguments()[0]] = Activator.CreateInstance(processorType) as QuestionProcessor;
-			}
-		}
-
-		protected abstract FormattedText ProcessInternal(KnowledgeBase knowledgeBase, Question question, ILanguage language);
+		public abstract FormattedText Process(KnowledgeBase knowledgeBase, Question question, ILanguage language);
 	}
 
 	public abstract class QuestionProcessor<QuestionT> : QuestionProcessor
@@ -45,16 +12,9 @@ namespace Inventor.Core
 	{
 		protected abstract FormattedText ProcessImplementation(KnowledgeBase knowledgeBase, QuestionT question, ILanguage language);
 
-		protected override FormattedText ProcessInternal(KnowledgeBase knowledgeBase, Question question, ILanguage language)
+		public override FormattedText Process(KnowledgeBase knowledgeBase, Question question, ILanguage language)
 		{
-			if (question.GetType() == typeof (QuestionT))
-			{
-				return ProcessImplementation(knowledgeBase, question as QuestionT, language);
-			}
-			else
-			{
-				throw new ArrayTypeMismatchException();
-			}
+			return ProcessImplementation(knowledgeBase, (QuestionT) question, language);
 		}
 	}
 }
