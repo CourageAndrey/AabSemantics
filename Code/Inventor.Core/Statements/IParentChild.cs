3e5d3018787dcb246c82f3cce22018679272ed6a
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-01-09 16:01:12 +0300

    Add explanation mechanism to IParentChild extensions.

--- Code/Inventor.Core/Statements/IParentChild.cs
+++ Code/Inventor.Core/Statements/IParentChild.cs
@@ -12,57 +12,57 @@ namespace Inventor.Core.Statements
 
 	public static class ParentChildHelper
 	{
-		public static List<T> GetParentsAllLevels<T, RelationshipT>(this IEnumerable<Statement> statements, T item)
+		public static List<T> GetParentsAllLevels<T, RelationshipT>(this IEnumerable<Statement> statements, T item, List<RelationshipT> involvedRelationships = null)
 			where RelationshipT : IParentChild<T>
 			where T : class
 		{
-			return GetParentsAllLevels(statements.OfType<RelationshipT>(), item);
+			return GetParentsAllLevels(statements.OfType<RelationshipT>(), item, involvedRelationships);
 		}
 
-		public static List<T> GetChildrenAllLevels<T, RelationshipT>(this IEnumerable<Statement> statements, T item)
+		public static List<T> GetChildrenAllLevels<T, RelationshipT>(this IEnumerable<Statement> statements, T item, List<RelationshipT> involvedRelationships = null)
 			where RelationshipT : IParentChild<T>
 			where T : class
 		{
-			return GetChildrenAllLevels(statements.OfType<RelationshipT>(), item);
+			return GetChildrenAllLevels(statements.OfType<RelationshipT>(), item, involvedRelationships);
 		}
 
-		public static List<T> GetParentsOneLevel<T, RelationshipT>(this IEnumerable<Statement> statements, T item)
+		public static List<T> GetParentsOneLevel<T, RelationshipT>(this IEnumerable<Statement> statements, T item, List<RelationshipT> involvedRelationships = null)
 			where RelationshipT : IParentChild<T>
 			where T : class
 		{
-			return GetParentsOneLevel(statements.OfType<RelationshipT>(), item);
+			return GetParentsOneLevel(statements.OfType<RelationshipT>(), item, involvedRelationships);
 		}
 
-		public static List<T> GetChildrenOnLevel<T, RelationshipT>(this IEnumerable<Statement> statements, T item)
+		public static List<T> GetChildrenOnLevel<T, RelationshipT>(this IEnumerable<Statement> statements, T item, List<RelationshipT> involvedRelationships = null)
 			where RelationshipT : IParentChild<T>
 			where T : class
 		{
-			return GetChildrenOnLevel(statements.OfType<RelationshipT>(), item);
+			return GetChildrenOnLevel(statements.OfType<RelationshipT>(), item, involvedRelationships);
 		}
 
-		public static List<T> GetParentsAllLevels<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item)
+		public static List<T> GetParentsAllLevels<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item, List<RelationshipT> involvedRelationships = null)
 			where RelationshipT : IParentChild<T>
 			where T : class
 		{
-			return GetRelatedAllLevels(relationships, item, GetParentsOneLevel);
+			return GetRelatedAllLevels(relationships, item, involvedRelationships, GetParentsOneLevel);
 		}
 
-		public static List<T> GetChildrenAllLevels<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item)
+		public static List<T> GetChildrenAllLevels<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item, List<RelationshipT> involvedRelationships = null)
 			where RelationshipT : IParentChild<T>
 			where T : class
 		{
-			return GetRelatedAllLevels(relationships, item, GetChildrenOnLevel);
+			return GetRelatedAllLevels(relationships, item, involvedRelationships, GetChildrenOnLevel);
 		}
 
-		private delegate List<T> GetRelativesDelegate<T, in RelationshipT>(IEnumerable<RelationshipT> relationships, T item);
+		private delegate List<T> GetRelativesDelegate<T, RelationshipT>(IEnumerable<RelationshipT> relationships, T item, List<RelationshipT> involvedRelationships = null);
 
-		private static List<T> GetRelatedAllLevels<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item, GetRelativesDelegate<T, RelationshipT> getRelatives)
+		private static List<T> GetRelatedAllLevels<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item, List<RelationshipT> involvedRelationships, GetRelativesDelegate<T, RelationshipT> getRelatives)
 		{
 			var result = new List<T>();
 			var relativesToCheck = new List<T> { item };
 			while (relativesToCheck.Count > 0)
 			{
-				var nextGeneration = relativesToCheck.Aggregate(new List<T>(), (list, relative) => { list.AddRange(getRelatives(relationships, relative)); return list; });
+				var nextGeneration = relativesToCheck.Aggregate(new List<T>(), (list, relative) => { list.AddRange(getRelatives(relationships, relative, involvedRelationships)); return list; });
 				nextGeneration.RemoveAll(result.Contains);
 				relativesToCheck = nextGeneration.Distinct().ToList();
 				result.AddRange(relativesToCheck);
@@ -70,18 +70,28 @@ namespace Inventor.Core.Statements
 			return result;
 		}
 
-		public static List<T> GetParentsOneLevel<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item)
+		public static List<T> GetParentsOneLevel<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item, List<RelationshipT> involvedRelationships = null)
 			where RelationshipT : IParentChild<T>
 			where T : class
 		{
-			return relationships.Where(c => c.Child == item).Select(c => c.Parent).ToList();
+			var foundRelationships = relationships.Where(c => c.Child == item).ToList();
+			if (involvedRelationships != null)
+			{
+				involvedRelationships.AddRange(foundRelationships);
+			}
+			return foundRelationships.Select(c => c.Parent).ToList();
 		}
 
-		public static List<T> GetChildrenOnLevel<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item)
+		public static List<T> GetChildrenOnLevel<T, RelationshipT>(this IEnumerable<RelationshipT> relationships, T item, List<RelationshipT> involvedRelationships = null)
 			where RelationshipT : IParentChild<T>
 			where T : class
 		{
-			return relationships.Where(c => c.Parent == item).Select(c => c.Child).ToList();
+			var foundRelationships = relationships.Where(c => c.Parent == item).ToList();
+			if (involvedRelationships != null)
+			{
+				involvedRelationships.AddRange(foundRelationships);
+			}
+			return foundRelationships.Select(c => c.Child).ToList();
 		}
 	}
 }
