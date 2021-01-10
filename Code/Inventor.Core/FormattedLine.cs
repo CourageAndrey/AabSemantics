6c2392e716c3796b4c45a5bf6b58a2a31c9055a5
Andrei Bychko <bublic-xiii@list.ru>
2021-01-10 17:20:45 +0300

    Extract interfaces: Move formatted strings classes to root folder.

---
+++ Code/Inventor.Core/FormattedLine.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace Inventor.Core
+{
+	public sealed class FormattedLine
+	{
+		#region Properties
+
+		public Func<string> Formatter
+		{ get { return _formatter; } }
+
+		public IDictionary<String, INamed> Parameters
+		{ get { return _parameters; } }
+
+		private readonly Func<String> _formatter;
+		private readonly IDictionary<String, INamed> _parameters;
+
+		#endregion
+
+		public FormattedLine(Func<String> formatter, IDictionary<String, INamed> parameters)
+		{
+			_formatter = formatter;
+			_parameters = new Dictionary<String, INamed>(parameters);
+		}
+
+		public String GetPlainText(ILanguage language)
+		{
+			String result = _formatter();
+			foreach (var parameter in _parameters)
+			{
+				result = result.Replace(parameter.Key, String.Format("\"{0}\"", parameter.Value.Name.GetValue(language)));
+			}
+			return result;
+		}
+
+		public String GetHtml(ILanguage language, Int32 lineNumber)
+		{
+			String result = _formatter();
+			foreach (var parameter in _parameters)
+			{
+				result = result.Replace(
+					parameter.Key,
+					String.Format("<a href=\"{0}\">{1}</a>", GetParam(lineNumber, parameter.Key), HttpUtility.HtmlEncode(parameter.Value.Name.GetValue(language))));
+			}
+			return result + @"<br/><br/>";
+		}
+
+		public static String GetParam(Int32 line, String param)
+		{
+			return String.Format("#{0:00000000}{1}", line, param.Remove(0, 1));
+		}
+	}
+}
