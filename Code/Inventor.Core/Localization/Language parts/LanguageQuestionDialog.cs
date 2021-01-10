b8345aa2c6730f08d3e1411e75496eb926ab6c38
Andrei Bychko <courage.andrey@gmail.com>
2021-01-10 11:42:09 +0300

    Move language classes to files with appropriate names.

---
+++ Code/Inventor.Core/Localization/Language parts/LanguageQuestionDialog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Inventor.Core.Localization
+{
+	public interface ILanguageQuestionDialog
+	{
+		String Title
+		{ get; }
+
+		String SelectQuestion
+		{ get; }
+	}
+
+	public class LanguageQuestionDialog : ILanguageQuestionDialog
+	{
+		#region Properties
+
+		[XmlElement]
+		public String Title
+		{ get; set; }
+
+		[XmlElement]
+		public String SelectQuestion
+		{ get; set; }
+
+		#endregion
+
+		internal static LanguageQuestionDialog CreateDefault()
+		{
+			return new LanguageQuestionDialog
+			{
+				Title = "Формирование вопроса",
+				SelectQuestion = "Выберите вопрос: ",
+			};
+		}
+	}
+}
