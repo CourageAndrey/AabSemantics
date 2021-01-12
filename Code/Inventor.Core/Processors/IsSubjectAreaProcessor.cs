59239f74a14937a339cc493a46c3811736384fa1
Andrei Bychko <bublic-xiii@list.ru>
2021-01-12 21:56:04 +0300

    Mark some question processors obsolete, because they duplicate functionality of CheckStatementProcessor.
    Marked classes:
     * IsPartOfProcessor
     * IsProcessor
     * IsSubjectAreaProcessor

--- Code/Inventor.Core/Processors/IsSubjectAreaProcessor.cs
+++ Code/Inventor.Core/Processors/IsSubjectAreaProcessor.cs
@@ -8,6 +8,7 @@ using Inventor.Core.Questions;
 
 namespace Inventor.Core.Processors
 {
+	[Obsolete("This class will be removed as soon as QuestionDialog supports CheckStatementQuestion. Please, use CheckStatementQuestion with corresponding statement instead.")]
 	public sealed class IsSubjectAreaProcessor : QuestionProcessor<IsSubjectAreaQuestion>
 	{
 		public override IAnswer Process(IQuestionProcessingContext<IsSubjectAreaQuestion> context)
