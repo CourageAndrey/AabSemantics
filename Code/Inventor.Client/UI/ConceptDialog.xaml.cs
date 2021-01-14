5c09de3d99a27ff6e4879e4fb657eec602caad07
Andrei Bychko <courage.andrey@gmail.com>
2021-01-14 11:15:18 +0300

    Implement possibility to add/edit/remove concepts.

---
+++ Code/Inventor.Client/UI/ConceptDialog.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace Inventor.Client.UI
+{
+	public partial class ConceptDialog
+	{
+		public ConceptDialog()
+		{
+			InitializeComponent();
+		}
+
+		public ViewModels.Concept EditValue
+		{
+			get { return _contextControl.DataContext as ViewModels.Concept; }
+			set
+			{
+				_contextControl.DataContext = value;
+				_nameConrol.IsEnabled = value.Name != null;
+				_hintConrol.IsEnabled = value.Hint != null;
+			}
+		}
+
+		private void okClick(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+
+		private void cancelClick(object sender, RoutedEventArgs e)
+		{
+			DialogResult = false;
+		}
+	}
+}
