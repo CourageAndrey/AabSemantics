bcea208d2a7fbc3acf4f707fd08ff61ed09f693b
Andrei Bychko <courage.andrey@gmail.com>
2021-01-22 21:50:33 +0300

    Make it possible to get result of StatementViewModel instantation.

--- Code/Inventor.Client/IViewModel.cs
+++ Code/Inventor.Client/IViewModel.cs
@@ -12,4 +12,31 @@ namespace Inventor.Client
 
 		void ApplyUpdate();
 	}
+
+	public abstract class StatementViewModel : IViewModel
+	{
+		public abstract Window CreateEditDialog(Window owner, IKnowledgeBase knowledgeBase, ILanguage language);
+
+		public void ApplyCreate(IKnowledgeBase knowledgeBase)
+		{
+			knowledgeBase.Statements.Add(CreateStatement());
+		}
+
+		public abstract void ApplyUpdate();
+
+		public abstract IStatement CreateStatement();
+	}
+
+	public abstract class StatementViewModel<StatementT> : StatementViewModel
+		where StatementT : IStatement
+	{
+		protected StatementT _boundObject;
+
+		public override IStatement CreateStatement()
+		{
+			return _boundObject = CreateStatementImplementation();
+		}
+
+		protected abstract StatementT CreateStatementImplementation();
+	}
 }
---
+++ Code/Inventor.Client/ViewModels/Factory.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+using Inventor.Client.TreeNodes;
+
+namespace Inventor.Client.ViewModels
+{
+	internal static class Factory
+	{
+		public static IViewModel CreateByCoreType(Type type)
+		{
+			if (type == typeof(Core.Base.Concept))
+			{
+				return new Concept();
+			}
+			else if (type == typeof(Core.Statements.ConsistsOfStatement))
+			{
+				return new ConsistsOfStatement();
+			}
+			else if (type == typeof(Core.Statements.GroupStatement))
+			{
+				return new GroupStatement();
+			}
+			else if (type == typeof(Core.Statements.HasSignStatement))
+			{
+				return new HasSignStatement();
+			}
+			else if (type == typeof(Core.Statements.IsStatement))
+			{
+				return new IsStatement();
+			}
+			else if (type == typeof(Core.Statements.SignValueStatement))
+			{
+				return new SignValueStatement();
+			}
+			else
+			{
+				throw new NotSupportedException(type.FullName);
+			}
+		}
+
+		public static StatementViewModel CreateStatementByInstance(Core.IStatement statement, Core.ILanguage language)
+		{
+			if (statement is Core.Statements.ConsistsOfStatement)
+			{
+				return new ConsistsOfStatement(statement as Core.Statements.ConsistsOfStatement, language);
+			}
+			else if (statement is Core.Statements.GroupStatement)
+			{
+				return new GroupStatement(statement as Core.Statements.GroupStatement, language);
+			}
+			else if (statement is Core.Statements.HasSignStatement)
+			{
+				return new HasSignStatement(statement as Core.Statements.HasSignStatement, language);
+			}
+			else if (statement is Core.Statements.IsStatement)
+			{
+				return new IsStatement(statement as Core.Statements.IsStatement, language);
+			}
+			else if (statement is Core.Statements.SignValueStatement)
+			{
+				return new SignValueStatement(statement as Core.Statements.SignValueStatement, language);
+			}
+			else
+			{
+				throw new NotSupportedException(statement.GetType().FullName);
+			}
+		}
+
+		public static IViewModel CreateByTreeNode(ExtendedTreeNode treeNode, Core.ILanguage language)
+		{
+			var conceptNode = treeNode as ConceptNode;
+			var statementNode = treeNode as StatementNode;
+
+			if (conceptNode != null)
+			{
+				return new Concept(conceptNode.Concept as Core.Base.Concept);
+			}
+			else if (statementNode != null)
+			{
+				if (statementNode.Statement is Core.Statements.ConsistsOfStatement)
+				{
+					return new ConsistsOfStatement(statementNode.Statement as Core.Statements.ConsistsOfStatement, language);
+				}
+				else if (statementNode.Statement is Core.Statements.GroupStatement)
+				{
+					return new GroupStatement(statementNode.Statement as Core.Statements.GroupStatement, language);
+				}
+				else if (statementNode.Statement is Core.Statements.HasSignStatement)
+				{
+					return new HasSignStatement(statementNode.Statement as Core.Statements.HasSignStatement, language);
+				}
+				else if (statementNode.Statement is Core.Statements.IsStatement)
+				{
+					return new IsStatement(statementNode.Statement as Core.Statements.IsStatement, language);
+				}
+				else if (statementNode.Statement is Core.Statements.SignValueStatement)
+				{
+					return new SignValueStatement(statementNode.Statement as Core.Statements.SignValueStatement, language);
+				}
+			}
+
+			throw new NotSupportedException(treeNode.GetType().FullName);
+		}
+	}
+}
