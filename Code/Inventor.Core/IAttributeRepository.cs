a80662c3889d73e838800908f83c55f9e369d27c
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-01-29 21:48:21 +0300

    Extract Attributes Repository.

---
+++ Code/Inventor.Core/IAttributeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inventor.Core
+{
+	public interface IAttributeRepository
+	{
+		IDictionary<Type, AttributeDefinition> AttributeDefinitions
+		{ get; }
+	}
+}
