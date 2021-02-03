b46c5e579738733f479845e7f909e4aba34737e8
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-02-03 13:54:19 +0300

    Add new system concepts, which will allow to change way, how comparison- and process-statements work.

--- Code/Inventor.Core/Localization/LanguageSystemConcepts.cs
+++ Code/Inventor.Core/Localization/LanguageSystemConcepts.cs
@@ -7,6 +7,8 @@ namespace Inventor.Core.Localization
 	{
 		#region Properties
 
+		#region Logical values
+
 		[XmlElement]
 		public String True
 		{ get; set; }
@@ -17,12 +19,104 @@ namespace Inventor.Core.Localization
 
 		#endregion
 
+		#region Comparison signs
+
+		[XmlElement]
+		public String IsEqualTo
+		{ get; set; }
+
+		[XmlElement]
+		public String IsNotEqualTo
+		{ get; set; }
+
+		[XmlElement]
+		public String IsGreaterThanOrEqualTo
+		{ get; set; }
+
+		[XmlElement]
+		public String IsGreaterThan
+		{ get; set; }
+
+		[XmlElement]
+		public String IsLessThanOrEqualTo
+		{ get; set; }
+
+		[XmlElement]
+		public String IsLessThan
+		{ get; set; }
+
+		#endregion
+
+		#region Sequence signs
+
+		[XmlElement]
+		public String StartsAfterOtherStarted
+		{ get; set; }
+
+		[XmlElement]
+		public String StartsBeforeOtherStarted
+		{ get; set; }
+
+		[XmlElement]
+		public String FinishesAfterOtherStarted
+		{ get; set; }
+
+		[XmlElement]
+		public String FinishesBeforeOtherStarted
+		{ get; set; }
+
+		[XmlElement]
+		public String StartsAfterOtherFinished
+		{ get; set; }
+
+		[XmlElement]
+		public String StartsBeforeOtherFinished
+		{ get; set; }
+
+		[XmlElement]
+		public String FinishesAfterOtherFinished
+		{ get; set; }
+
+		[XmlElement]
+		public String FinishesBeforeOtherFinished
+		{ get; set; }
+
+		[XmlElement]
+		public String Causes
+		{ get; set; }
+
+		[XmlElement]
+		public String SimultaneousWith
+		{ get; set; }
+
+		#endregion
+
+		#endregion
+
 		internal static LanguageSystemConcepts CreateDefaultNames()
 		{
 			return new LanguageSystemConcepts
 			{
 				True = "Да",
 				False = "Нет",
+
+				IsEqualTo = " = ",
+				IsNotEqualTo = " ≠ ",
+				IsGreaterThanOrEqualTo = " ≥ ",
+				IsGreaterThan = " > ",
+				IsLessThanOrEqualTo = " ≤ ",
+				IsLessThan = " < ",
+
+				StartsAfterOtherStarted = " начинается после начала ",
+				StartsBeforeOtherStarted = " начинается до начала ",
+				FinishesAfterOtherStarted = " завершается после начала ",
+				FinishesBeforeOtherStarted = " завершается до начала ",
+				StartsAfterOtherFinished = " начинается после завершения ",
+				StartsBeforeOtherFinished = " начинается до завершения ",
+				FinishesAfterOtherFinished = " завершается после завершения ",
+				FinishesBeforeOtherFinished = " завершается до завершения ",
+				Causes = " вызывает ",
+				SimultaneousWith = " идёт одновременно с ",
 			};
 		}
 
@@ -32,6 +126,24 @@ namespace Inventor.Core.Localization
 			{
 				True = "Логическое значение: истина.",
 				False = "Логическое значение: ложь.",
+
+				IsEqualTo = "Сравнение: равно.",
+				IsNotEqualTo = "Сравнение: не равно.",
+				IsGreaterThanOrEqualTo = "Сравнение: больше или равно.",
+				IsGreaterThan = "Сравнение: больше.",
+				IsLessThanOrEqualTo = "Сравнение: меньше или равно.",
+				IsLessThan = "Сравнение: меньше.",
+
+				StartsAfterOtherStarted = "Процессы: начинается после начала ...",
+				StartsBeforeOtherStarted = "Процессы: начинается до начала ...",
+				FinishesAfterOtherStarted = "Процессы: завершается после начала ...",
+				FinishesBeforeOtherStarted = "Процессы: завершается до начала ...",
+				StartsAfterOtherFinished = "Процессы: начинается после завершения ...",
+				StartsBeforeOtherFinished = "Процессы: начинается до завершения ...",
+				FinishesAfterOtherFinished = "Процессы: завершается после завершения ...",
+				FinishesBeforeOtherFinished = "Процессы: завершается до завершения ...",
+				Causes = "Процессы: вызывает ...",
+				SimultaneousWith = "Процессы: идёт одновременно с ...",
 			};
 		}
 	}
--- Code/Inventor.Core/SystemConcepts.cs
+++ Code/Inventor.Core/SystemConcepts.cs
@@ -28,11 +28,117 @@ namespace Inventor.Core
 
 		#endregion
 
+		#region Comparison signs
+
+		public static readonly IConcept IsEqualTo = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.IsEqualTo),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.IsEqualTo));
+
+		public static readonly IConcept IsNotEqualTo = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.IsNotEqualTo),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.IsNotEqualTo));
+
+		public static readonly IConcept IsGreaterThanOrEqualTo = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.IsGreaterThanOrEqualTo),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.IsGreaterThanOrEqualTo));
+
+		public static readonly IConcept IsGreaterThan = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.IsGreaterThan),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.IsGreaterThan));
+
+		public static readonly IConcept IsLessThanOrEqualTo = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.IsLessThanOrEqualTo),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.IsLessThanOrEqualTo));
+
+		public static readonly IConcept IsLessThan = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.IsLessThan),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.IsLessThan));
+
+		public static readonly ICollection<IConcept> ComparisonSigns = new HashSet<IConcept>
+		{
+			IsEqualTo,
+			IsNotEqualTo,
+			IsGreaterThanOrEqualTo,
+			IsGreaterThan,
+			IsLessThanOrEqualTo,
+			IsLessThan,
+		};
+
+		#endregion
+
+		#region Sequence signs
+
+		public static readonly IConcept StartsAfterOtherStarted = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.StartsAfterOtherStarted),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.StartsAfterOtherStarted));
+
+		public static readonly IConcept StartsBeforeOtherStarted = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.StartsBeforeOtherStarted),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.StartsBeforeOtherStarted));
+
+		public static readonly IConcept FinishesAfterOtherStarted = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.FinishesAfterOtherStarted),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.FinishesAfterOtherStarted));
+
+		public static readonly IConcept FinishesBeforeOtherStarted = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.FinishesBeforeOtherStarted),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.FinishesBeforeOtherStarted));
+
+		public static readonly IConcept StartsAfterOtherFinished = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.StartsAfterOtherFinished),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.StartsAfterOtherFinished));
+
+		public static readonly IConcept StartsBeforeOtherFinished = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.StartsBeforeOtherFinished),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.StartsBeforeOtherFinished));
+
+		public static readonly IConcept FinishesAfterOtherFinished = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.FinishesAfterOtherFinished),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.FinishesAfterOtherFinished));
+
+		public static readonly IConcept FinishesBeforeOtherFinished = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.FinishesBeforeOtherFinished),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.FinishesBeforeOtherFinished));
+
+		public static readonly IConcept Causes = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.Causes),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.Causes));
+
+		public static readonly IConcept SimultaneousWith = new Concept(
+			new LocalizedStringConstant(lang => lang.SystemConceptNames.SimultaneousWith),
+			new LocalizedStringConstant(lang => lang.SystemConceptHints.SimultaneousWith));
+
+		public static readonly ICollection<IConcept> SequenceSigns = new HashSet<IConcept>
+		{
+			StartsAfterOtherStarted,
+			StartsBeforeOtherStarted,
+			FinishesAfterOtherStarted,
+			FinishesBeforeOtherStarted,
+			StartsAfterOtherFinished,
+			StartsBeforeOtherFinished,
+			FinishesAfterOtherFinished,
+			FinishesBeforeOtherFinished,
+			Causes,
+			SimultaneousWith,
+		};
+
+		#endregion
+
 		#endregion
 
 		public static IEnumerable<IConcept> GetAll()
 		{
 			foreach (var concept in LogicalValues)
+			{
+				yield return concept;
+			}
+
+			foreach (var concept in ComparisonSigns)
+			{
+				yield return concept;
+			}
+
+			foreach (var concept in SequenceSigns)
 			{
 				yield return concept;
 			}
