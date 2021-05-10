f5449e811910a3141d91c6ce5aea789fc19d0be0
Andrei Bychko <courage.andrey@gmail.com>
2021-05-10 08:52:41 +0300

    Implement non-transient version of Comparison Question.

---
+++ Code/Inventor.Core/Processors/ComparisonQuestionProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Inventor.Core.Answers;
+using Inventor.Core.Base;
+using Inventor.Core.Localization;
+using Inventor.Core.Statements;
+using Inventor.Core.Questions;
+
+namespace Inventor.Core.Processors
+{
+	public sealed class ComparisonQuestionProcessor : QuestionProcessor<ComparisonQuestion, ComparisonStatement>
+	{
+		protected override IAnswer CreateAnswer(IQuestionProcessingContext<ComparisonQuestion> context, ICollection<ComparisonStatement> statements)
+		{
+			if (statements.Any())
+			{
+				var statement = statements.First();
+				return new ConceptAnswer(
+					statement.ComparisonSign,
+					new FormattedText(() => context.Language.Answers.CompareValues, new Dictionary<String, INamed>
+					{
+						{ Strings.ParamLeftValue, statement.LeftValue },
+						{ Strings.ParamComparisonSign, statement.ComparisonSign },
+						{ Strings.ParamRightValue, statement.RightValue },
+					}),
+					new Explanation(statements));
+			}
+			else
+			{
+				return Answer.CreateUnknown(context.Language);
+			}
+		}
+
+		protected override bool DoesStatementMatch(IQuestionProcessingContext<ComparisonQuestion> context, ComparisonStatement statement)
+		{
+			return statement.LeftValue == context.Question.LeftValue && statement.RightValue == context.Question.RightValue;
+		}
+	}
+}
---
+++ Code/Inventor.Core/Questions/ComparisonQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Inventor.Core.Questions
+{
+	public sealed class ComparisonQuestion : IQuestion
+	{
+		public IConcept LeftValue
+		{ get; set; }
+
+		public IConcept RightValue
+		{ get; set; }
+
+		public ComparisonQuestion(IConcept leftValue, IConcept rightValue)
+		{
+			if (leftValue == null) throw new ArgumentNullException(nameof(leftValue));
+			if (rightValue == null) throw new ArgumentNullException(nameof(rightValue));
+
+			LeftValue = leftValue;
+			RightValue = rightValue;
+		}
+	}
+}
