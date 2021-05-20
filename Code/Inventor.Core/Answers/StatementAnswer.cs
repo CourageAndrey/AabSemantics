1fa0a928e0cc6d54cee69a73f22448bf76c24a1d
Andrei Bychko <bublic-xiii@list.ru>
2021-05-20 10:38:23 +0300

    Introduce StatementAnswer class.

---
+++ Code/Inventor.Core/Answers/StatementAnswer.cs
@@ -0,0 +1,18 @@
+﻿namespace Inventor.Core.Answers
+{
+	public class StatementAnswer : Answer, IAnswer<IStatement>
+	{
+		#region Properties
+
+		public IStatement Result
+		{ get; }
+
+		#endregion
+
+		public StatementAnswer(IStatement result, FormattedText description, IExplanation explanation)
+			: base(description, explanation, result == null)
+		{
+			Result = result;
+		}
+	}
+}
