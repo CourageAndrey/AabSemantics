da84d57e6a1e3d4570433071b24c9653fd88d456
Andrei Bychko <courage.andrey@gmail.com>
2021-06-10 23:21:26 +0300

    Cover DescribeSubjectAreaProcessor with unit tests.

---
+++ Code/Inventor.Test/Processors/DescribeSubjectAreaProcessorTest.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using Inventor.Core;
+using Inventor.Core.Answers;
+using Inventor.Core.Base;
+using Inventor.Core.Localization;
+using Inventor.Core.Questions;
+using Inventor.Core.Statements;
+
+namespace Inventor.Test.Processors
+{
+	[TestFixture]
+	public class DescribeSubjectAreaProcessorTest
+	{
+		[Test]
+		public void SubjectAreaWithoutConceptsHasNoConcepts()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new TestKnowledgeBase(language);
+			var noSubjectAreaConcept = SystemConcepts.GetAll().First();
+
+			// act
+			var question = new DescribeSubjectAreaQuestion(noSubjectAreaConcept);
+			var answer = question.Ask(knowledgeBase.KnowledgeBase.Context);
+
+			// assert
+			Assert.IsTrue(answer.IsEmpty);
+			Assert.AreEqual(0, answer.Explanation.Statements.Count);
+		}
+
+		[Test]
+		public void SubjectAreaWithConceptCanFindIt()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new TestKnowledgeBase(language);
+
+			var area = SystemConcepts.True;
+			var concept = SystemConcepts.False;
+			var groupStatement = new GroupStatement(area, concept);
+			knowledgeBase.KnowledgeBase.Statements.Add(groupStatement);
+
+			// act
+			var question = new DescribeSubjectAreaQuestion(area);
+			var answer = question.Ask(knowledgeBase.KnowledgeBase.Context);
+
+			// assert
+			Assert.IsFalse(answer.IsEmpty);
+
+			Assert.AreSame(groupStatement, answer.Explanation.Statements.Single());
+
+			var conceptsAnswer = (ConceptsAnswer) answer;
+			Assert.AreEqual(concept, conceptsAnswer.Result.Single());
+		}
+
+		[Test]
+		public void MultipleConcepts()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new TestKnowledgeBase(language);
+
+			// act
+			var question = new DescribeSubjectAreaQuestion(knowledgeBase.SubjectArea_Transport);
+			var answer = question.Ask(knowledgeBase.KnowledgeBase.Context);
+
+			// assert
+			Assert.IsFalse(answer.IsEmpty);
+
+			var conceptsAnswer = (ConceptsAnswer) answer;
+			Assert.Greater(conceptsAnswer.Result.Count, 0);
+
+			var groupStatements = answer.Explanation.Statements.OfType<GroupStatement>().ToList();
+			Assert.AreEqual(answer.Explanation.Statements.Count, groupStatements.Count);
+
+			Assert.IsTrue(groupStatements.All(statement => statement.Area == knowledgeBase.SubjectArea_Transport));
+			Assert.AreEqual(groupStatements.Count, conceptsAnswer.Result.Count);
+			Assert.AreEqual(groupStatements.Count, groupStatements.Select(statement => statement.Concept).Intersect(conceptsAnswer.Result).Count());
+		}
+	}
+}
