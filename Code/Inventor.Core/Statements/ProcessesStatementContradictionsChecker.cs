034aecdcc381913621c6fd39499d6afef6f3bb25
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-07-22 16:42:24 +0300

    Split large ContradictionsChecker.cs file.

---
+++ Code/Inventor.Core/Statements/ProcessesStatementContradictionsChecker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inventor.Core.Statements
+{
+	public class ProcessesStatementContradictionsChecker : ContradictionsChecker<ProcessesStatement>
+	{
+		public ProcessesStatementContradictionsChecker(IEnumerable<ProcessesStatement> statements)
+			: base(statements)
+		{ }
+
+		protected override IConcept GetLeftValue(ProcessesStatement statement)
+		{
+			return statement.ProcessA;
+		}
+
+		protected override IConcept GetRightValue(ProcessesStatement statement)
+		{
+			return statement.ProcessB;
+		}
+
+		protected override IConcept GetSign(ProcessesStatement statement)
+		{
+			return statement.SequenceSign;
+		}
+
+		protected override Boolean SetCombinationWithDescendants(IConcept valueRow, IConcept valueColumn, IConcept sign)
+		{
+			Boolean combinationsUpdated = SetCombination(valueRow, valueColumn, sign);
+			combinationsUpdated |= SetCombination(valueColumn, valueRow, SequenceSigns.Revert(sign));
+			return combinationsUpdated;
+		}
+
+		protected override Boolean Contradicts(HashSet<IConcept> signs, IConcept left, IConcept right)
+		{
+			return signs.Contradicts();
+		}
+
+		protected override Boolean TryToUpdateCombinations(IConcept valueRow, IConcept signRow, IConcept signColumn, IConcept valueColumn)
+		{
+			var resultSign = SequenceSigns.TryToCombineMutualSequences(signRow, signColumn);
+			return resultSign != null && SetCombinationWithDescendants(valueRow, valueColumn, resultSign);
+		}
+	}
+}
