59c16fe783af009c1b5c3ff9d3eca6a6687e2a0b
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-07-23 15:50:31 +0300

    Cover CheckStatementProcessor with unit tests.

---
+++ Code/Inventor.Test/Processors/CheckStatementProcessorTest.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+using Inventor.Core;
+using Inventor.Core.Answers;
+using Inventor.Core.Base;
+using Inventor.Core.Localization;
+using Inventor.Core.Questions;
+
+namespace Inventor.Test.Processors
+{
+	[TestFixture]
+	public class CheckStatementProcessorTest
+	{
+		[Test]
+		public void ReturnEmptyAnswerIfNoMathingStatementsFound()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new KnowledgeBase(language);
+			var statementToCheck = new TestStatement(1);
+			var statementWrong = new TestStatement(2);
+			var question = new CheckStatementQuestion(statementToCheck);
+
+			// act
+			var answerNoStatements = question.Ask(knowledgeBase.Context);
+
+			knowledgeBase.Statements.Add(statementWrong);
+			var answerWrongStatement = question.Ask(knowledgeBase.Context);
+
+			// assert
+			var answer = (BooleanAnswer) answerNoStatements;
+			Assert.IsFalse(answer.IsEmpty);
+			Assert.IsFalse(answer.Result);
+			Assert.AreEqual(0, answer.Explanation.Statements.Count);
+
+			answer = (BooleanAnswer) answerWrongStatement;
+			Assert.IsFalse(answer.IsEmpty);
+			Assert.IsFalse(answer.Result);
+			Assert.AreEqual(0, answer.Explanation.Statements.Count);
+		}
+
+		[Test]
+		public void ReturnFoundStatementIfExists()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new KnowledgeBase(language);
+			var statementToCheck = new TestStatement(1);
+			var statementRight = new TestStatement(1);
+			var statementWrong = new TestStatement(2);
+			knowledgeBase.Statements.Add(statementRight);
+			knowledgeBase.Statements.Add(statementWrong);
+
+			// act
+			var answerToCheck = new CheckStatementQuestion(statementToCheck).Ask(knowledgeBase.Context);
+
+			var answerRight = new CheckStatementQuestion(statementRight).Ask(knowledgeBase.Context);
+
+			// assert
+			Assert.IsFalse(answerToCheck.IsEmpty);
+			Assert.IsTrue(((BooleanAnswer) answerToCheck).Result);
+			Assert.AreSame(statementRight, answerToCheck.Explanation.Statements.Single());
+
+			Assert.IsFalse(answerRight.IsEmpty);
+			Assert.IsTrue(((BooleanAnswer) answerRight).Result);
+			Assert.AreSame(statementRight, answerRight.Explanation.Statements.Single());
+		}
+
+		[Test]
+		public void SupportTransitiveStatements()
+		{
+			// arrange
+			var language = Language.Default;
+
+			var knowledgeBase = new KnowledgeBase(language);
+			var conceptParent = new Concept(new LocalizedStringConstant(l => "parent"));
+			var conceptIntermediate = new Concept(new LocalizedStringConstant(l => "intermediate"));
+			var conceptChild = new Concept(new LocalizedStringConstant(l => "child"));
+			knowledgeBase.Concepts.Add(conceptParent);
+			knowledgeBase.Concepts.Add(conceptIntermediate);
+			knowledgeBase.Concepts.Add(conceptChild);
+
+			var statementPI = new TransitiveTestStatement(conceptParent, conceptIntermediate);
+			var statementIC = new TransitiveTestStatement(conceptIntermediate, conceptChild);
+			knowledgeBase.Statements.Add(statementPI);
+			knowledgeBase.Statements.Add(statementIC);
+
+			var statementToCheck = new TransitiveTestStatement(conceptParent, conceptChild);
+			var question = new CheckStatementQuestion(statementToCheck);
+
+			// act
+			var answer = question.Ask(knowledgeBase.Context);
+
+			// assert
+			Assert.IsFalse(answer.IsEmpty);
+			Assert.IsTrue(((BooleanAnswer) answer).Result);
+			Assert.AreEqual(2, answer.Explanation.Statements.Count);
+			Assert.IsTrue(answer.Explanation.Statements.Contains(statementPI));
+			Assert.IsTrue(answer.Explanation.Statements.Contains(statementIC));
+		}
+
+		private class TestStatement : Statement<TestStatement>
+		{
+			public int Number
+			{ get; }
+
+			public TestStatement(int number)
+				: base(new LocalizedStringConstant(l => number.ToString()), new LocalizedStringConstant(l => number.ToString()))
+			{
+				Number = number;
+			}
+
+			public override IEnumerable<IConcept> GetChildConcepts()
+			{
+				yield break;
+			}
+
+			protected override Func<string> GetDescriptionText(ILanguageStatements language)
+			{
+				return () => string.Empty;
+			}
+
+			protected override IDictionary<string, INamed> GetDescriptionParameters()
+			{
+				return new Dictionary<string, INamed>();
+			}
+
+			public override bool Equals(TestStatement other)
+			{
+				return Number == other.Number;
+			}
+		}
+
+		private class TransitiveTestStatement : Statement<TransitiveTestStatement>, IParentChild<IConcept>
+		{
+
+			public IConcept Parent
+			{ get; }
+
+			public IConcept Child
+			{ get; }
+
+			public TransitiveTestStatement(IConcept parent, IConcept child)
+				: base(new LocalizedStringConstant(l => string.Empty), new LocalizedStringConstant(l => string.Empty))
+			{
+				Parent = parent;
+				Child = child;
+			}
+
+			public override bool Equals(TransitiveTestStatement other)
+			{
+				return Parent == other.Parent && Child == other.Child;
+			}
+
+			public override IEnumerable<IConcept> GetChildConcepts()
+			{
+				yield return Parent;
+				yield return Child;
+			}
+
+			protected override Func<string> GetDescriptionText(ILanguageStatements language)
+			{
+				return () => string.Empty;
+			}
+
+			protected override IDictionary<string, INamed> GetDescriptionParameters()
+			{
+				return new Dictionary<string, INamed>();
+			}
+		}
+	}
+}
