f010e88005ef174e1d626552dbe090752116a2e6
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-07-24 17:55:54 +0300

    Cover IsSignProcessor and IsValueProcessor with unit tests.

---
+++ Code/Inventor.Test/Processors/IsValueProcessorTest.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using Inventor.Core;
+using Inventor.Core.Answers;
+using Inventor.Core.Attributes;
+using Inventor.Core.Base;
+using Inventor.Core.Localization;
+using Inventor.Core.Questions;
+using Inventor.Core.Statements;
+
+namespace Inventor.Test.Processors
+{
+	[TestFixture]
+	public class IsValueProcessorTest
+	{
+		[Test]
+		public void ReturnFalseIfNoAttribute()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new TestKnowledgeBase(language).KnowledgeBase;
+
+			var concept = new Concept();
+			knowledgeBase.Concepts.Add(concept);
+
+			var question = new IsValueQuestion(concept);
+
+			// act
+			var answer = question.Ask(knowledgeBase.Context);
+
+			// assert
+			Assert.IsFalse(answer.IsEmpty);
+			Assert.IsFalse(((BooleanAnswer) answer).Result);
+			Assert.AreEqual(0, answer.Explanation.Statements.Count);
+		}
+
+		[Test]
+		public void ReturnTrueWithoutExplanationIfNoRelationships()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new TestKnowledgeBase(language).KnowledgeBase;
+
+			var concept = new Concept();
+			concept.Attributes.Add(IsValueAttribute.Value);
+			knowledgeBase.Concepts.Add(concept);
+
+			var question = new IsValueQuestion(concept);
+
+			// act
+			var answer = question.Ask(knowledgeBase.Context);
+
+			// assert
+			Assert.IsFalse(answer.IsEmpty);
+			Assert.IsTrue(((BooleanAnswer) answer).Result);
+			Assert.AreEqual(0, answer.Explanation.Statements.Count);
+		}
+
+		[Test]
+		public void ReturnTrueWithtExplanation()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new TestKnowledgeBase(language);
+
+			var question = new IsValueQuestion(knowledgeBase.MotorType_Combusion);
+
+			// act
+			var answer = question.Ask(knowledgeBase.KnowledgeBase.Context);
+
+			// assert
+			Assert.IsFalse(answer.IsEmpty);
+			Assert.IsTrue(((BooleanAnswer) answer).Result);
+			Assert.IsTrue(answer.Explanation.Statements.OfType<SignValueStatement>().Any());
+		}
+	}
+}
