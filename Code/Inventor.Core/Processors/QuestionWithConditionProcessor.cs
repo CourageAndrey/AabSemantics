9d2e5223a95f7c157031502ac3c13a6321cacdf0
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-07-24 18:05:12 +0300

    Mark QuestionWithConditionProcessor as obsolete.

--- Code/Inventor.Core/Processors/QuestionWithConditionProcessor.cs
+++ Code/Inventor.Core/Processors/QuestionWithConditionProcessor.cs
@@ -1,8 +1,11 @@
-﻿using Inventor.Core.Base;
+﻿using System;
+
+using Inventor.Core.Base;
 using Inventor.Core.Questions;
 
 namespace Inventor.Core.Processors
 {
+	[Obsolete("As all questions have their own precondition list, this class becomes unusable. It will be deleted after UI will support proconditions for all questions.")]
 	public sealed class QuestionWithConditionProcessor : QuestionProcessor<QuestionWithCondition>
 	{
 		public override IAnswer Process(IQuestionProcessingContext<QuestionWithCondition> context)
