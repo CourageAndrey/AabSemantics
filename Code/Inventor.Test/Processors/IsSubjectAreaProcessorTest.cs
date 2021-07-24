6a529712fd9928e57aeed00b2361da65ed9e2d71
Andrei Bychko <courage.andrey@gmail.com>
2021-07-24 18:57:34 +0300

    Cover IsPartOfProcessor and IsSubjectAreaProcessor with unit tests.

---
+++ Code/Inventor.Test/Processors/IsSubjectAreaProcessorTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using Inventor.Core;
+using Inventor.Core.Answers;
+using Inventor.Core.Base;
+using Inventor.Core.Localization;
+using Inventor.Core.Questions;
+using Inventor.Core.Statements;
+
+namespace Inventor.Test.Processors
+{
+	public class IsSubjectAreaProcessorTest
+	{
+		[Test]
+		public void ReturnFalseIfNotFound()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new TestKnowledgeBase(language);
+
+			var question = new IsSubjectAreaQuestion(knowledgeBase.SubjectArea_Numbers, knowledgeBase.Base_Vehicle);
+
+			// act
+			var answer = question.Ask(knowledgeBase.KnowledgeBase.Context);
+
+			// assert
+			Assert.IsFalse(answer.IsEmpty);
+			Assert.IsFalse(((BooleanAnswer) answer).Result);
+			Assert.AreEqual(0, answer.Explanation.Statements.Count);
+		}
+
+		[Test]
+		public void ReturnTrueIfFound()
+		{
+			// arrange
+			var language = Language.Default;
+			var knowledgeBase = new TestKnowledgeBase(language);
+
+			var question = new IsSubjectAreaQuestion(knowledgeBase.Base_Vehicle, knowledgeBase.SubjectArea_Transport);
+
+			// act
+			var answer = question.Ask(knowledgeBase.KnowledgeBase.Context);
+
+			// assert
+			Assert.IsFalse(answer.IsEmpty);
+			Assert.IsTrue(((BooleanAnswer) answer).Result);
+
+			var statement = (GroupStatement) answer.Explanation.Statements.Single();
+			Assert.IsTrue(statement.Concept == knowledgeBase.Base_Vehicle && statement.Area == knowledgeBase.SubjectArea_Transport);
+		}
+	}
+}
