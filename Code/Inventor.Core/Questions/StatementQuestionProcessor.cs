b5bc17feae722dd9a09a55352b4318838d7e5dc4
Andrei Bychko <courage.andrey@gmail.com>
2021-07-29 10:50:57 +0300

    Extract StatementQuestionProcessor class.

---
+++ Code/Inventor.Core/Questions/StatementQuestionProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inventor.Core.Questions
+{
+	public class StatementQuestionProcessor<QuestionT, StatementT>
+		where QuestionT : Question<QuestionT, StatementT>
+		where StatementT : IStatement
+	{
+		#region Properties
+
+		public IQuestionProcessingContext<QuestionT> Context
+		{ get; }
+
+		public ICollection<StatementT> Statements
+		{ get; }
+
+		#endregion
+
+		public StatementQuestionProcessor(IQuestionProcessingContext<QuestionT> context, Func<StatementT, Boolean> match)
+		{
+			Context = context;
+
+			Statements = context.KnowledgeBase.Statements
+				.Enumerate<StatementT>(context.ActiveContexts)
+				.Where(match)
+				.ToList();
+		}
+	}
+}
