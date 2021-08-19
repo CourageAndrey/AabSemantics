3b9d8e9036ef5fe682122655bc9be9ee58be0bfe
Andrei Bychko <courage.andrey@gmail.com>
2021-08-19 14:03:28 +0300

    Improve INamed.Enumerate extensions.

--- Code/Inventor.Core/INamed.cs
+++ Code/Inventor.Core/INamed.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Inventor.Core
@@ -12,31 +13,12 @@ namespace Inventor.Core
 
 	public static class NamedHelper
 	{
-		public static Dictionary<String , INamed> Enumerate(this IList<INamed> list, out String format)
-		{
-			var formatText = new StringBuilder();
-			var paremeters = new Dictionary<String , INamed>();
-			for (Int32 i = 0; i < list.Count; i++)
-			{
-				String param = String .Format("#ENUMITEM{0:00000000}#", i);
-				paremeters[param] = list[i];
-				formatText.Append(param);
-				if (i != list.Count - 1)
-				{
-					formatText.Append(", ");
-				}
-			}
-			format = formatText.ToString();
-			return paremeters;
-		}
-
-		public static Dictionary<String , INamed> Enumerate<T>(this IEnumerable<T> amount, out String format)
-			where T : INamed
+		public static Dictionary<String , INamed> Enumerate(this IEnumerable<INamed> namedItems, out String format)
 		{
 			var formatText = new StringBuilder();
 			var paremeters = new Dictionary<String , INamed>();
 			Int32 index = 0;
-			foreach (var item in amount)
+			foreach (var item in namedItems)
 			{
 				if (index > 0)
 				{
@@ -49,6 +31,12 @@ namespace Inventor.Core
 			}
 			format = formatText.ToString();
 			return paremeters;
+		}
+
+		public static Dictionary<String , INamed> Enumerate<T>(this IEnumerable<T> namedItems, out String format)
+			where T : INamed
+		{
+			return namedItems.OfType<INamed>().Enumerate(out format);
 		}
 	}
 }
