90bb786e62d592c70c02b1f52cdb027f5305b742
Andrei Bychko <courage.andrey@gmail.com>
2021-08-23 16:01:11 +0300

    Simplify NamedHelper extensions.

--- Code/Inventor.Core/INamed.cs
+++ Code/Inventor.Core/INamed.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Inventor.Core
 {
@@ -13,30 +12,19 @@ namespace Inventor.Core
 
 	public static class NamedHelper
 	{
-		public static Dictionary<String , INamed> Enumerate(this IEnumerable<INamed> namedItems, out String format)
+		public static Dictionary<String , INamed> Enumerate(this IEnumerable<IKnowledge> knowledgeItems, out String format)
 		{
-			var formatText = new StringBuilder();
-			var paremeters = new Dictionary<String , INamed>();
-			Int32 index = 0;
-			foreach (var item in namedItems)
-			{
-				if (index > 0)
-				{
-					formatText.Append(", ");
-				}
-				String param = String .Format("#ENUMITEM{0:00000000}#", index);
-				paremeters[param] = item;
-				formatText.Append(param);
-				index++;
-			}
-			format = formatText.ToString();
-			return paremeters;
+			var parameters = knowledgeItems.ToDictionary(
+				k => $"#{k.ID}#",
+				k => k as INamed);
+			format = String.Join(", ", parameters.Keys);
+			return parameters;
 		}
 
-		public static Dictionary<String , INamed> Enumerate<T>(this IEnumerable<T> namedItems, out String format)
-			where T : INamed
+		public static Dictionary<String , INamed> Enumerate<T>(this IEnumerable<T> knowledgeItems, out String format)
+			where T : IKnowledge
 		{
-			return namedItems.OfType<INamed>().Enumerate(out format);
+			return knowledgeItems.OfType<IKnowledge>().Enumerate(out format);
 		}
 	}
 }
