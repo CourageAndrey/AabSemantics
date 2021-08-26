1f7c96f01d1b4589975dda038162e7e8abb27d58
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-08-26 20:20:35 +0300

    Cover AttributeDefinition with unit tests and fix it according to their result.

--- Code/Inventor.Core/AttributeDefinition.cs
+++ Code/Inventor.Core/AttributeDefinition.cs
@@ -20,6 +20,11 @@ namespace Inventor.Core
 
 		public AttributeDefinition(Type attributeType, IAttribute attributeValue, Func<ILanguage, String> attributeNameGetter)
 		{
+			if (attributeType == null) throw new ArgumentNullException(nameof(attributeType));
+			if (attributeValue == null) throw new ArgumentNullException(nameof(attributeValue));
+			if (!attributeType.IsInstanceOfType(attributeValue)) throw new InvalidCastException();
+			if (attributeNameGetter == null) throw new ArgumentNullException(nameof(attributeNameGetter));
+
 			AttributeType = attributeType;
 			AttributeValue = attributeValue;
 			_attributeNameGetter = attributeNameGetter;
@@ -32,6 +37,9 @@ namespace Inventor.Core
 			return _attributeNameGetter(language);
 		}
 
-		public static readonly AttributeDefinition None = new AttributeDefinition(null, null, language => language.Attributes.None);
+		public static readonly AttributeDefinition None = new AttributeDefinition(typeof(NoAttribute), new NoAttribute(), language => language.Attributes.None);
+
+		private class NoAttribute : IAttribute
+		{ }
 	}
 }
