420141869b7a0caac104164b27915f5a5206d016
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-08-29 10:13:53 +0300

    Add GetAnchor extension to IKnowledge interface.

--- Code/Inventor.Core/IKnowledge.cs
+++ Code/Inventor.Core/IKnowledge.cs
@@ -1,8 +1,18 @@
-﻿namespace Inventor.Core
+﻿using System;
+
+namespace Inventor.Core
 {
 	public interface IKnowledge : INamed, IIdentifiable
 	{
 		ILocalizedString Hint
 		{ get; }
+	}
+
+	public static class KnowledgeHelper
+	{
+		public static String GetAnchor(this IKnowledge knowledge)
+		{
+			return $"#{knowledge.ID}#";
+		}
 	}
 }
