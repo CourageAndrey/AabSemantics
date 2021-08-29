b89d19381856e5a07f966b3e3608d3264ee43d95
Andrei Bychko <courage.andrey@gmail.com>
2021-08-29 10:13:48 +0300

    Implement other required text extensions.

--- Code/Inventor.Core/IText.cs
+++ Code/Inventor.Core/IText.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Inventor.Core.Text;
 
@@ -24,6 +25,18 @@ namespace Inventor.Core
 
 	public static class TextExtensions
 	{
+		#region Concatenation methods
+
+		public static ITextContainer Append(this IText text1, IText text2)
+		{
+			var container = text1 as UnstructuredContainer;
+			if (container == null)
+			{
+				container = new UnstructuredContainer(text1);
+			}
+			return container.Append(text2);
+		}
+
 		public static ITextContainer Append(this ITextContainer textContainer, IText line)
 		{
 			textContainer.Items.Add(line);
@@ -42,6 +55,68 @@ namespace Inventor.Core
 			return textContainer;
 		}
 
-#warning Add other extensions.
+		public static ITextContainer AppendSpace(this ITextContainer textContainer)
+		{
+			textContainer.Items.Add(new SpaceText());
+			return textContainer;
+		}
+
+		public static ITextContainer AppendBulletsList(this ITextContainer textContainer, IEnumerable<IText> items)
+		{
+			textContainer.Items.Add(new BulletsContainer(items.ToList()));
+			return textContainer;
+		}
+
+		public static ITextContainer AppendNumberingList(this ITextContainer textContainer, IEnumerable<IText> items)
+		{
+			textContainer.Items.Add(new NumberingContainer(items.ToList()));
+			return textContainer;
+		}
+
+		#endregion
+
+		#region Decorations
+
+		public static BoldDecorator MakeBold(this IText text)
+		{
+			return new BoldDecorator(text);
+		}
+
+		public static ItalicDecorator MakeItalic(this IText text)
+		{
+			return new ItalicDecorator(text);
+		}
+
+		public static UnderlineDecorator MakeUnderline(this IText text)
+		{
+			return new UnderlineDecorator(text);
+		}
+
+		public static StrikeoutDecorator MakeStrikeout(this IText text)
+		{
+			return new StrikeoutDecorator(text);
+		}
+
+		public static SubscriptDecorator MakeSubscript(this IText text)
+		{
+			return new SubscriptDecorator(text);
+		}
+
+		public static SuperscriptDecorator MakeSuperscript(this IText text)
+		{
+			return new SuperscriptDecorator(text);
+		}
+
+		public static HeaderDecorator MakeHeader(this IText text, Byte level)
+		{
+			return new HeaderDecorator(text, level);
+		}
+
+		public static ParagraphDecorator MakeParagraph(this IText text)
+		{
+			return new ParagraphDecorator(text);
+		}
+
+		#endregion
 	}
 }
