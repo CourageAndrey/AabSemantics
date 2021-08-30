bcedd3227a652571dce6e24d1f9a8a6a3e2a4920
Andrei Bychko <courage.andrey@gmail.com>
2021-08-30 11:42:04 +0300

    Add custom Question sample.

---
+++ Code/Samples.04.CustomQuestion/SelectRandomConceptQuestion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+using Inventor.Core;
+using Inventor.Core.Answers;
+using Inventor.Core.Base;
+using Inventor.Core.Questions;
+using Inventor.Core.Text;
+
+namespace Samples._04.CustomQuestion
+{
+	public class SelectRandomConceptQuestion : Question
+	{
+		public override IAnswer Process(IQuestionProcessingContext context)
+		{
+			var random = new Random(DateTime.Now.Millisecond);
+			var allConcepts = context.SemanticNetwork.Concepts.ToList();
+			var resultConcept = allConcepts[random.Next(allConcepts.Count)];
+
+			return new ConceptAnswer(
+				resultConcept,
+				new FormattedText(language => resultConcept.Name.GetValue(language)),
+				new Explanation(Array.Empty<IStatement>()));
+		}
+	}
+
+	public static class SubjectQuestionExtensions
+	{
+		public static IAnswer ForRandomConcept(this QuestionBuilder builder)
+		{
+			var question = new SelectRandomConceptQuestion();
+			return question.Ask(builder.SemanticNetwork.Context);
+		}
+	}
+}
