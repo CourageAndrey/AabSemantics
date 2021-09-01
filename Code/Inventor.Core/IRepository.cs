24a1650ab956f889ae413c37869984b434fc592b
Andrei Bychko <courage.andrey@gmail.com>
2021-09-01 23:23:40 +0300

    Merge DefinitionRepositoryHelper into IRepository extensions.

--- Code/Inventor.Core/IRepository.cs
+++ Code/Inventor.Core/IRepository.cs
@@ -17,5 +17,23 @@ namespace Inventor.Core
 		{
 			repository.Definitions[questionDefinition.Type] = questionDefinition;
 		}
+
+		public static DefinitionT GetSuitable<InstanceT, DefinitionT>(this IDictionary<Type, DefinitionT> repository, InstanceT instance)
+		{
+			var type = instance.GetType();
+
+			while (type != null)
+			{
+				DefinitionT definition;
+				if (repository.TryGetValue(type, out definition))
+				{
+					return definition;
+				}
+
+				type = type.BaseType;
+			}
+
+			throw new NotSupportedException();
+		}
 	}
 }
