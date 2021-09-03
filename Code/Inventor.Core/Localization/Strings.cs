915e9ceb6ca3b2feb12507299727815c3a9a0f97
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-09-03 14:41:12 +0300

    Remove unused Strings.TostringFormatted constant.

--- Code/Inventor.Core/Localization/Strings.cs
+++ Code/Inventor.Core/Localization/Strings.cs
@@ -5,7 +5,6 @@ namespace Inventor.Core.Localization
 	internal static class Strings
 	{
 		public const String TostringSemanticNetwork = "KNOWLEDGE_BASE";
-		public const String TostringFormatted = "FORMATTED_STRING";
 		public const String TostringLocalized = "LOCALIZED_STRING";
 
 		public const String ParamConcept = "#CONCEPT#";
