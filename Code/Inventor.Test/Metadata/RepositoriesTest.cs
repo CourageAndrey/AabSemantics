da38974c9670a54d5f7a3634a6f6b1066f34f044
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-09-05 12:43:54 +0300

    Cover Repositories class with unit tests.

---
+++ Code/Inventor.Test/Metadata/RepositoriesTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Inventor.Core;
+using Inventor.Core.Metadata;
+
+namespace Inventor.Test.Metadata
+{
+	[TestFixture]
+	public class RepositoriesTest
+	{
+		[Test]
+		public void ImpossibleToSetNullModules()
+		{
+			Assert.Throws<ArgumentNullException>(() => Repositories.Modules = null);
+
+			Assert.DoesNotThrow(() => Repositories.Modules = new Dictionary<string, IExtensionModule>());
+		}
+
+		[Test]
+		public void ImpossibleToSetNullAttributes()
+		{
+			Assert.Throws<ArgumentNullException>(() => Repositories.Attributes = null);
+
+			Assert.DoesNotThrow(() => Repositories.Attributes = new Repository<AttributeDefinition>());
+		}
+
+		[Test]
+		public void ImpossibleToSetNullStatements()
+		{
+			Assert.Throws<ArgumentNullException>(() => Repositories.Statements = null);
+
+			Assert.DoesNotThrow(() => Repositories.Statements = new Repository<StatementDefinition>());
+		}
+
+		[Test]
+		public void ImpossibleToSetNullQuestions()
+		{
+			Assert.Throws<ArgumentNullException>(() => Repositories.Questions = null);
+
+			Assert.DoesNotThrow(() => Repositories.Questions = new Repository<QuestionDefinition>());
+		}
+	}
+}
