db308d48894c1ff0db6de1f86be85b40b111e97b
Andrei Bychko <courage.andrey@gmail.com>
2021-10-28 16:38:14 +0300

    Cover Huffman class with unit tests (and fix found bugs).

---
+++ Code/Tests/Inventor.Algorithms.Test/HuffmanTest.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace Inventor.Algorithms.Test
+{
+	[TestFixture]
+	public class HuffmanTest
+	{
+		[Test]
+		public void GivenNullItemsWhenTryToEncodeThenThrowError()
+		{
+			// act & assert
+			Assert.Throws<ArgumentNullException>(() => Huffman.HuffmanEncode<SimpleNode, string>(null, getWeight, string.Empty, appendLeft, appendRight));
+		}
+
+		[Test]
+		public void GivenNullGetWeightWhenTryToEncodeThenThrowError()
+		{
+			// arrange
+			var items = new List<SimpleNode> { new SimpleNode() };
+
+			// act & assert
+			Assert.Throws<ArgumentNullException>(() => items.HuffmanEncode(null, string.Empty, appendLeft, appendRight));
+		}
+
+		[Test]
+		public void GivenNullAppentLeftWhenTryToEncodeThenThrowError()
+		{
+			// arrange
+			var items = new List<SimpleNode> { new SimpleNode() };
+
+			// act & assert
+			Assert.Throws<ArgumentNullException>(() => items.HuffmanEncode(getWeight, string.Empty, null, appendRight));
+		}
+
+		[Test]
+		public void GivenNullAppendRightWhenTryToEncodeThenThrowError()
+		{
+			// arrange
+			var items = new List<SimpleNode> { new SimpleNode() };
+
+			// act & assert
+			Assert.Throws<ArgumentNullException>(() => items.HuffmanEncode(getWeight, string.Empty, appendLeft, null));
+		}
+
+		[Test]
+		public void Given0ItemsWhenEncodeThenReturnEmpty()
+		{
+			// arrange
+			var items = new List<SimpleNode>();
+
+			// act
+			var codes = items.HuffmanEncode(getWeight, string.Empty, appendLeft, appendRight);
+
+			// assert
+			Assert.AreEqual(0, codes.Count);
+		}
+
+		[Test]
+		public void Given1ItemWhenEncodeThenReturnSimple()
+		{
+			// arrange
+			var items = new List<SimpleNode> { new SimpleNode() };
+
+			// act
+			var codes = items.HuffmanEncode(getWeight, string.Empty, appendLeft, appendRight);
+
+			// assert
+			Assert.AreSame(items[0], codes.Keys.Single());
+		}
+
+		[Test]
+		public void Given2ItemsWithSameWeightWhenEncodeThenReturnSimple()
+		{
+			// arrange
+			var items = new List<SimpleNode>
+			{
+				new SimpleNode("L", 1),
+				new SimpleNode("R", 1),
+			};
+
+			// act
+			var codes = items.HuffmanEncode(getWeight, string.Empty, appendLeft, appendRight);
+
+			// assert
+			Assert.AreEqual(2, codes.Count);
+			foreach (var item in items)
+			{
+				Assert.AreEqual(item.Name, codes[item]);
+			}
+		}
+
+		[Test]
+		public void Given2ItemsWithDifferentWeightWhenEncodeThenReturnSimple()
+		{
+			// arrange
+			var items = new List<SimpleNode>
+			{
+				new SimpleNode("L", 1),
+				new SimpleNode("R", 1000),
+			};
+
+			// act
+			var codes = items.HuffmanEncode(getWeight, string.Empty, appendLeft, appendRight);
+
+			// assert
+			Assert.AreEqual(2, codes.Count);
+			foreach (var item in items)
+			{
+				Assert.AreEqual(item.Name, codes[item]);
+			}
+		}
+
+		[Test]
+		public void GivenItemsWithTheSameWeightWhenEncodeThenReturnBalanceTree()
+		{
+			// arrange
+			var items = new List<SimpleNode>
+			{
+				new SimpleNode("LLL", 1),
+				new SimpleNode("LLR", 1),
+				new SimpleNode("LRL", 1),
+				new SimpleNode("LRR", 1),
+				new SimpleNode("RLL", 1),
+				new SimpleNode("RLR", 1),
+				new SimpleNode("RRL", 1),
+				new SimpleNode("RRR", 1),
+			};
+
+			// act
+			var codes = items.HuffmanEncode(getWeight, string.Empty, appendLeft, appendRight);
+
+			// assert
+			Assert.AreEqual(8, codes.Count);
+			foreach (var item in items)
+			{
+				Assert.AreEqual(item.Name, codes[item]);
+			}
+		}
+
+		[Test]
+		public void GivenItemsWithDisperseWeightWhenEncodeThenReturnOneSideTree()
+		{
+			// arrange
+			var items = new List<SimpleNode>
+			{
+				new SimpleNode("LLL", 1),
+				new SimpleNode("LLR", 10),
+				new SimpleNode("LR", 100),
+				new SimpleNode("R", 1000),
+			};
+
+			// act
+			var codes = items.HuffmanEncode(getWeight, string.Empty, appendLeft, appendRight);
+
+			// assert
+			Assert.AreEqual(4, codes.Count);
+			foreach (var item in items)
+			{
+				Assert.AreEqual(item.Name, codes[item]);
+			}
+		}
+
+		private static ulong getWeight(SimpleNode item)
+		{
+			return item.Weight;
+		}
+
+		private static string appendLeft(string code)
+		{
+			return code + "L";
+		}
+
+		private static string appendRight(string code)
+		{
+			return code + "R";
+		}
+
+		private class SimpleNode
+		{
+			public string Name
+			{ get; }
+
+			public ulong Weight
+			{ get; }
+
+			public SimpleNode(string name, ulong weight)
+			{
+				Name = name;
+				Weight = weight;
+			}
+
+			public SimpleNode()
+				: this(string.Empty, 0)
+			{ }
+
+			public override string ToString()
+			{
+				return $"{Name}:{Weight}";
+			}
+		}
+	}
+}
