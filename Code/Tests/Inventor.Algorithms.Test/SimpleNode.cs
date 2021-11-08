5f2666061e19b2e87ee7f19a3ea3cd5e82761cf6
Andrei Bychko <courage.andrey@gmail.com>
2021-11-08 15:59:24 +0200

    Move SimpleNode class to separate file in order to reuse it.

---
+++ Code/Tests/Inventor.Algorithms.Test/SimpleNode.cs
@@ -0,0 +1,22 @@
+﻿namespace Inventor.Algorithms.Test
+{
+	internal class SimpleNode
+	{
+		public string Name
+		{ get; }
+
+		public SimpleNode(string name)
+		{
+			Name = name;
+		}
+
+		public SimpleNode()
+			: this(string.Empty)
+		{ }
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
