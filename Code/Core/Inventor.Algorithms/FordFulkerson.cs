35ec8d9f60942bca9261f726dd61423fc3515e3e
Andrei Bychko <courage.andrey@gmail.com>
2021-11-08 17:29:16 +0200

    Implement Ford-Fulkerson graph algorithm.

---
+++ Code/Core/Inventor.Algorithms/FordFulkerson.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inventor.Algorithms
+{
+	public static class FordFulkerson
+	{
+		public static Double FindMaximumFlow<NodeT>(this ICollection<IArcWithFlow<NodeT>> arcs, NodeT sourceNode, NodeT destinationNode)
+			where NodeT : class
+		{
+			if (arcs == null) throw new ArgumentNullException(nameof(arcs));
+			if (sourceNode == null) throw new ArgumentNullException(nameof(sourceNode));
+			if (destinationNode == null) throw new ArgumentNullException(nameof(destinationNode));
+
+			arcs = new List<IArcWithFlow<NodeT>>(arcs);
+			Double maxFlow = 0;
+
+			var path = getPath(arcs, sourceNode, destinationNode);
+			while (path != null)
+			{
+				Double flow = path.Min(arc => arc.Flow);
+				maxFlow += flow;
+
+				foreach (var arc in path)
+				{
+					arcs.Remove(arc);
+
+					Double arcFlow = arc.Flow - flow;
+					if (arcFlow > Double.Epsilon)
+					{
+						arcs.Add(new SimpleArcWithFlow<NodeT>(arc, arcFlow));
+					}
+				}
+
+				path = getPath(arcs, sourceNode, destinationNode);
+			}
+
+			return maxFlow;
+		}
+
+		private static List<IArcWithFlow<NodeT>> getPath<NodeT>(ICollection<IArcWithFlow<NodeT>> arcs, NodeT sourceNode,  NodeT destinationNode)
+			where NodeT : class
+		{
+			var pathsToCheck = arcs.Where(arc => arc.From == sourceNode && arc.To != sourceNode).Select(arc => new List<IArcWithFlow<NodeT>> { arc }).ToList();
+
+			var visitedNodes = new HashSet<NodeT>(pathsToCheck.Select(path => path[0].To)) { sourceNode };
+
+			while (pathsToCheck.Count > 0)
+			{
+				foreach (var path in pathsToCheck)
+				{
+					if (path[path.Count - 1].To == destinationNode)
+					{
+						return path;
+					}
+				}
+
+				var nextStepPaths = new List<List<IArcWithFlow<NodeT>>>();
+				foreach (var path in pathsToCheck)
+				{
+					var lastNode = path[path.Count - 1].To;
+					foreach (var arc in arcs.Where(a => a.From == lastNode && !visitedNodes.Contains(a.To)))
+					{
+						nextStepPaths.Add(new List<IArcWithFlow<NodeT>>(path) { arc });
+					}
+				}
+				pathsToCheck = nextStepPaths;
+			}
+
+			return null;
+		}
+
+		public static Double FindMaximumFlow<NodeT>(this ICollection<IArcWithFlow<NodeT>> arcs)
+			where NodeT : class
+		{
+			if (arcs == null) throw new ArgumentNullException(nameof(arcs));
+
+			var sourceNodes = new HashSet<NodeT>();
+			var destinationNodes = new HashSet<NodeT>();
+			foreach (var arc in arcs)
+			{
+				sourceNodes.Add(arc.From);
+				destinationNodes.Add(arc.To);
+			}
+
+			NodeT sourceNode = null;
+			foreach (var node in sourceNodes)
+			{
+				if (!destinationNodes.Remove(node))
+				{
+					if (sourceNode == null)
+					{
+						sourceNode = node;
+					}
+					else
+					{
+						throw new ArgumentException("Only one source node (without inputs) allowed.");
+					}
+				}
+			}
+			if (sourceNode == null)
+			{
+				throw new ArgumentException("No source node (without inputs) found.");
+			}
+
+			if (destinationNodes.Count == 1)
+			{
+				var destinationNode = destinationNodes.First();
+				return FindMaximumFlow(arcs, sourceNode, destinationNode);
+			}
+			else if (destinationNodes.Count > 1)
+			{
+				throw new ArgumentException("Only one destination node (without outputs) allowed.");
+			}
+			else
+			{
+				throw new ArgumentException("No destination node (without outputs) found.");
+			}
+		}
+	}
+
+	public interface IArcWithFlow<out NodeT> : IArc<NodeT>
+	{
+		Double Flow
+		{ get; }
+	}
+
+	public class SimpleArcWithFlow<NodeT> : IArcWithFlow<NodeT>
+	{
+		#region Properties
+
+		public NodeT From
+		{ get; }
+
+		public NodeT To
+		{ get; }
+
+		public Double Flow
+		{ get; }
+
+		#endregion
+
+		public SimpleArcWithFlow(NodeT from, NodeT to, Double flow)
+		{
+			From = from;
+			To = to;
+			Flow = flow;
+		}
+
+		public SimpleArcWithFlow(IArcWithFlow<NodeT> arc, Double flow)
+		{
+			From = arc.From;
+			To = arc.To;
+			Flow = flow;
+		}
+	}
+}
---
+++ Code/Tests/Inventor.Algorithms.Test/FordFulkersonTest.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace Inventor.Algorithms.Test
+{
+	[TestFixture]
+	public class FordFulkersonTest
+	{
+		[Test]
+		public void GivenNullArcsWhenTryToFindMaximumFlowThenThrowError()
+		{
+			// arrange
+			var source = new SimpleNode("START");
+			var destination = new SimpleNode("DESTINATION");
+
+			// act & assert
+			Assert.Throws<ArgumentNullException>(() => FordFulkerson.FindMaximumFlow(null, source, destination));
+			Assert.Throws<ArgumentNullException>(() => FordFulkerson.FindMaximumFlow<IArcWithFlow<SimpleNode>>(null));
+		}
+
+		[Test]
+		public void GivenNullSourceWhenTryToFindMaximumFlowThenThrowError()
+		{
+			// arrange
+			var arcs = new List<IArcWithFlow<SimpleNode>>();
+			var destination = new SimpleNode("DESTINATION");
+
+			// act & assert
+			Assert.Throws<ArgumentNullException>(() => arcs.FindMaximumFlow(null, destination));
+		}
+
+		[Test]
+		public void GivenNullDestinationWhenTryToFindMaximumFlowThenThrowError()
+		{
+			// arrange
+			var arcs = new List<IArcWithFlow<SimpleNode>>();
+			var source = new SimpleNode("START");
+
+			// act & assert
+			Assert.Throws<ArgumentNullException>(() => arcs.FindMaximumFlow(source, null));
+		}
+
+		[Test]
+		public void GivenOneArcWhenFindMaximumFlowThenReturnItsFlow()
+		{
+			// arrange
+			var source = new SimpleNode("START");
+			var destination = new SimpleNode("DESTINATION");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(source, destination, 123),
+			};
+
+			// act
+			double maxFlow = arcs.FindMaximumFlow(source, destination);
+
+			// assert
+			Assert.AreEqual(123, maxFlow);
+		}
+
+		[Test]
+		public void GivenLinearChainWhenFindMaximumFlowThenReturnMinimumFlow()
+		{
+			// arrange
+			var a = new SimpleNode("A");
+			var b = new SimpleNode("B");
+			var c = new SimpleNode("C");
+			var d = new SimpleNode("D");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(a, b, 567),
+				new SimpleArcWithFlow<SimpleNode>(b, c, 123),
+				new SimpleArcWithFlow<SimpleNode>(c, d, 234),
+			};
+
+			// act
+			double maxFlow = arcs.FindMaximumFlow(a, d);
+
+			// assert
+			Assert.AreEqual(123, maxFlow);
+		}
+
+		[Test]
+		public void GivenParallelArcsWhenFindMaximumFlowThenReturnTheirSummary()
+		{
+			// arrange
+			var source = new SimpleNode("START");
+			var destination = new SimpleNode("DESTINATION");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(source, destination, 123),
+				new SimpleArcWithFlow<SimpleNode>(source, destination, 456),
+				new SimpleArcWithFlow<SimpleNode>(source, destination, 789),
+			};
+
+			// act
+			double maxFlow = arcs.FindMaximumFlow(source, destination);
+
+			// assert
+			Assert.AreEqual(123 + 456 + 789, maxFlow);
+		}
+
+		[Test]
+		public void GivenComplexGraphWhenFindMaximumFlowThenReturnIt()
+		{
+			// arrange
+			var a = new SimpleNode("A");
+			var b = new SimpleNode("B");
+			var c = new SimpleNode("C");
+			var d = new SimpleNode("D");
+			var e = new SimpleNode("E");
+			var f = new SimpleNode("F");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(a, b, 7),
+				new SimpleArcWithFlow<SimpleNode>(a, c, 4),
+				new SimpleArcWithFlow<SimpleNode>(b, c, 2),
+				new SimpleArcWithFlow<SimpleNode>(b, e, 4),
+				new SimpleArcWithFlow<SimpleNode>(c, d, 4),
+				new SimpleArcWithFlow<SimpleNode>(c, e, 8),
+				new SimpleArcWithFlow<SimpleNode>(d, f, 12),
+				new SimpleArcWithFlow<SimpleNode>(e, d, 4),
+				new SimpleArcWithFlow<SimpleNode>(e, f, 5),
+			};
+
+			// act
+			double maxFlow = arcs.FindMaximumFlow(a, f);
+
+			// assert
+			Assert.AreEqual(10, maxFlow);
+		}
+
+		/*[Test]
+		public void Given1111111111111111WhenFindMaximumFlowThenReturn222222222222222()
+		{
+			// arrange
+			var source = new SimpleNode("START");
+			var destination = new SimpleNode("DESTINATION");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(, , ),
+			};
+
+			// act
+			double maxFlow = arcs.FindMaximumFlow(source, destination);
+
+			// assert
+			Assert.AreEqual(, maxFlow);
+		}*/
+
+		[Test]
+		public void GivenNoArcsWhenTryToFindMaximumFlowThenThrowError()
+		{
+			// arrange
+			var arcs = new List<IArcWithFlow<SimpleNode>>();
+
+			// act & assert
+			Assert.Throws<ArgumentException>(() => arcs.FindMaximumFlow());
+		}
+
+		[Test]
+		public void GivenNoSourceLoopsWhenTryToFindMaximumFlowThenThrowError()
+		{
+			// arrange
+			var a = new SimpleNode("A");
+			var b = new SimpleNode("B");
+			var c = new SimpleNode("C");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(a, b, 1),
+				new SimpleArcWithFlow<SimpleNode>(a, c, 1),
+				new SimpleArcWithFlow<SimpleNode>(c, a, 1),
+			};
+
+			// act & assert
+			Assert.Throws<ArgumentException>(() => arcs.FindMaximumFlow());
+		}
+
+		[Test]
+		public void GivenNoDestinationLoopsWhenTryToFindMaximumFlowThenThrowError()
+		{
+			// arrange
+			var a = new SimpleNode("A");
+			var b = new SimpleNode("B");
+			var c = new SimpleNode("C");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(a, b, 1),
+				new SimpleArcWithFlow<SimpleNode>(b, c, 1),
+				new SimpleArcWithFlow<SimpleNode>(c, b, 1),
+			};
+
+			// act & assert
+			Assert.Throws<ArgumentException>(() => arcs.FindMaximumFlow());
+		}
+
+		[Test]
+		public void GivenCycledLoopsWhenTryToFindMaximumFlowThenThrowError()
+		{
+			// arrange
+			var a = new SimpleNode("A");
+			var b = new SimpleNode("B");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(a, b, 1),
+				new SimpleArcWithFlow<SimpleNode>(b, a, 1),
+			};
+
+			// act & assert
+			Assert.Throws<ArgumentException>(() => arcs.FindMaximumFlow());
+		}
+
+		[Test]
+		public void GivenTwoSourcesWhenTryToFindMaximumFlowThenThrowError()
+		{
+			// arrange
+			var source1 = new SimpleNode("START_1");
+			var source2 = new SimpleNode("START_2");
+			var destination = new SimpleNode("DESTINATION");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(source1, destination, 1),
+				new SimpleArcWithFlow<SimpleNode>(source2, destination, 1),
+			};
+
+			// act & assert
+			Assert.Throws<ArgumentException>(() => arcs.FindMaximumFlow());
+		}
+
+		[Test]
+		public void GivenTwoDestinationsWhenTryToFindMaximumFlowThenThrowError()
+		{
+			// arrange
+			var source = new SimpleNode("START");
+			var destination1 = new SimpleNode("DESTINATION_1");
+			var destination2 = new SimpleNode("DESTINATION_2");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(source, destination1, 1),
+				new SimpleArcWithFlow<SimpleNode>(source, destination2, 1),
+			};
+
+			// act & assert
+			Assert.Throws<ArgumentException>(() => arcs.FindMaximumFlow());
+		}
+
+		[Test]
+		public void GivenSingleSourceSingleDestinationWhenFindMaximumFlowThenWorksFine()
+		{
+			// arrange
+			var source = new SimpleNode("START");
+			var destination = new SimpleNode("DESTINATION");
+
+			var arcs = new List<IArcWithFlow<SimpleNode>>
+			{
+				new SimpleArcWithFlow<SimpleNode>(source, destination, 1),
+			};
+
+			// act & assert
+			Assert.DoesNotThrow(() => arcs.FindMaximumFlow());
+		}
+	}
+}
