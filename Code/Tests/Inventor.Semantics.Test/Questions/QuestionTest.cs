ee7efe931046a20b8bc901c666fc313da8077df1
Andrei Bychko <courage.andrey@gmail.com>
2022-09-12 13:34:14 +0300

    Cover NullArgument-checks within Question constructors with unit tests.

--- Code/Tests/Inventor.Semantics.Test/Questions/QuestionTest.cs
+++ Code/Tests/Inventor.Semantics.Test/Questions/QuestionTest.cs
@@ -1,16 +1,22 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using NUnit.Framework;
 
 using Inventor.Semantics.Answers;
 using Inventor.Semantics.Concepts;
 using Inventor.Semantics.Localization;
-using Inventor.Semantics.Questions;
-using Inventor.Semantics.Statements;
 using Inventor.Semantics.Mathematics.Concepts;
 using Inventor.Semantics.Mathematics.Questions;
 using Inventor.Semantics.Mathematics.Statements;
 using Inventor.Semantics.Modules.Boolean.Attributes;
+using Inventor.Semantics.Modules.Boolean.Questions;
+using Inventor.Semantics.Modules.Classification.Questions;
+using Inventor.Semantics.Processes.Questions;
+using Inventor.Semantics.Questions;
+using Inventor.Semantics.Set.Questions;
+using Inventor.Semantics.Statements;
 
 namespace Inventor.Semantics.Test.Questions
 {
@@ -57,6 +63,49 @@ namespace Inventor.Semantics.Test.Questions
 			Assert.IsTrue(answerWithPreconditions.Explanation.Statements.Contains(preconditionComparison));
 
 			Assert.AreSame(initialComparison, semanticNetwork.Statements.Single());
+		}
+
+		[Test]
+		[TestCaseSource(nameof(createQuestionsArgumentNullException))]
+		public void CheckQuestionsArgumentNullExceptions(Func<IQuestion> constructor)
+		{
+			Assert.Throws<ArgumentNullException>(() => constructor());
+		}
+
+		private static IEnumerable<object[]> createQuestionsArgumentNullException()
+		{
+			IConcept concept = "test".CreateConcept();
+
+			yield return new object[] { new Func<IQuestion>(() => new CheckStatementQuestion(null)) };
+			yield return new object[] { new Func<IQuestion>(() => new ComparisonQuestion(null, concept)) };
+			yield return new object[] { new Func<IQuestion>(() => new ComparisonQuestion(concept, null)) };
+			yield return new object[] { new Func<IQuestion>(() => new DescribeSubjectAreaQuestion(null)) };
+			yield return new object[] { new Func<IQuestion>(() => new EnumerateAncestorsQuestion(null)) };
+			yield return new object[] { new Func<IQuestion>(() => new EnumerateContainersQuestion(null)) };
+			yield return new object[] { new Func<IQuestion>(() => new EnumerateDescendantsQuestion(null)) };
+			yield return new object[] { new Func<IQuestion>(() => new EnumeratePartsQuestion(null)) };
+			yield return new object[] { new Func<IQuestion>(() => new EnumerateSignsQuestion(null, false)) };
+			yield return new object[] { new Func<IQuestion>(() => new FindSubjectAreaQuestion(null)) };
+			yield return new object[] { new Func<IQuestion>(() => new GetCommonQuestion(null, concept)) };
+			yield return new object[] { new Func<IQuestion>(() => new GetCommonQuestion(concept, null)) };
+			yield return new object[] { new Func<IQuestion>(() => new GetDifferencesQuestion(null, concept)) };
+			yield return new object[] { new Func<IQuestion>(() => new GetDifferencesQuestion(concept, null)) };
+			yield return new object[] { new Func<IQuestion>(() => new HasSignQuestion(null, concept, false)) };
+			yield return new object[] { new Func<IQuestion>(() => new HasSignQuestion(concept, null, false)) };
+			yield return new object[] { new Func<IQuestion>(() => new HasSignsQuestion(null, false)) };
+			yield return new object[] { new Func<IQuestion>(() => new IsPartOfQuestion(null, concept)) };
+			yield return new object[] { new Func<IQuestion>(() => new IsPartOfQuestion(concept, null)) };
+			yield return new object[] { new Func<IQuestion>(() => new IsQuestion(null, concept)) };
+			yield return new object[] { new Func<IQuestion>(() => new IsQuestion(concept, null)) };
+			yield return new object[] { new Func<IQuestion>(() => new IsSignQuestion(null)) };
+			yield return new object[] { new Func<IQuestion>(() => new IsSubjectAreaQuestion(null, concept)) };
+			yield return new object[] { new Func<IQuestion>(() => new IsSubjectAreaQuestion(concept, null)) };
+			yield return new object[] { new Func<IQuestion>(() => new IsValueQuestion(null)) };
+			yield return new object[] { new Func<IQuestion>(() => new ProcessesQuestion(null, concept)) };
+			yield return new object[] { new Func<IQuestion>(() => new ProcessesQuestion(concept, null)) };
+			yield return new object[] { new Func<IQuestion>(() => new SignValueQuestion(null, concept)) };
+			yield return new object[] { new Func<IQuestion>(() => new SignValueQuestion(concept, null)) };
+			yield return new object[] { new Func<IQuestion>(() => new WhatQuestion(null)) };
 		}
 	}
 }
