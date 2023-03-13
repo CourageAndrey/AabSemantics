2d72713ceaab92ea4277d5ecfa72007519a2990d
Andrei Bychko <courage.andrey@gmail.com>
2023-03-13 14:14:37 +0200

    Draft

---
+++ Code/Core/Inventor.Semantics/Answers/IsomorphicSearchAnswer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using Inventor.Semantics.Mutations;
+
+namespace Inventor.Semantics.Answers
+{
+	public class IsomorphicSearchAnswer : Answer, IAnswer<ICollection<KnowledgeStructure>>
+	{
+		#region Properties
+
+		public ICollection<KnowledgeStructure> Result
+		{ get; }
+
+		#endregion
+
+		public IsomorphicSearchAnswer(ICollection<KnowledgeStructure> result, IText description, IExplanation explanation)
+			: base(description, explanation, result.Count == 0)
+		{
+			Result = result;
+		}
+	}
+}
---
+++ Code/Core/Inventor.Semantics/Questions/IsomorphicSearchQuestion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Inventor.Semantics.Answers;
+using Inventor.Semantics.Mutations;
+using Inventor.Semantics.Questions;
+using Inventor.Semantics.Text.Containers;
+
+namespace Inventor.Semantics.Questions
+{
+	public class IsomorphicSearchQuestion : Question
+	{
+		#region Properties
+
+		public IsomorphicSearchPattern SearchPattern
+		{ get; set; }
+
+		public Boolean SelectMany
+		{ get; set; }
+
+		#endregion
+
+		public IsomorphicSearchQuestion(IsomorphicSearchPattern searchPattern, Boolean selectMany = false, IEnumerable<IStatement> preconditions = null)
+			: base(preconditions)
+		{
+			if (searchPattern == null) throw new ArgumentNullException(nameof(searchPattern));
+
+			SearchPattern = searchPattern;
+			SelectMany = false;
+		}
+
+		public override IAnswer Process(IQuestionProcessingContext context)
+		{
+			ICollection<KnowledgeStructure> foundKnowledge;
+
+			if (SelectMany)
+			{
+				foundKnowledge = SearchPattern.FindMatches(context.SemanticNetwork).ToList();
+			}
+			else
+			{
+				var match = context.SemanticNetwork.FindFirstMatch(SearchPattern);
+				foundKnowledge = match != null
+					? new[] { match }
+					: Array.Empty<KnowledgeStructure>();
+			}
+
+			return new IsomorphicSearchAnswer(
+				foundKnowledge,
+				,
+				);
+		}
+	}
+}
