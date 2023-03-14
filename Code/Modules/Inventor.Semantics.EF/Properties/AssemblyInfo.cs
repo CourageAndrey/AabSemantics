55e355dedc23faa13af1277d241c4f9c86ddb51f
Andrei Bychko <andrei.a.bychko@gmail.com>
2023-03-14 13:27:41 +0200

    Add possibility to work with DB-hosted semantic network via EntityFramework.

---
+++ Code/Modules/Inventor.Semantics.EF/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Inventor.Semantics.EF")]
+[assembly: AssemblyDescription("Inventor.Semantics.EF")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andrei Bychko")]
+[assembly: AssemblyProduct("Inventor")]
+[assembly: AssemblyCopyright("Copyright © Andrei Bychko 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d9b375a5-9d66-490a-ac5c-4b153749c7d5")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
