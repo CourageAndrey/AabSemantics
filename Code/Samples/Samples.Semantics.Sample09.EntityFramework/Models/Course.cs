a4d947a907771c202b738dee9c269e0d32f5c0f1
Andrei Bychko <courage.andrey@gmail.com>
2023-03-14 13:27:55 +0200

    Add EntityFramework-related sample.

---
+++ Code/Samples/Samples.Semantics.Sample09.EntityFramework/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Samples.Semantics.Sample09.EntityFramework.Models
+{
+	public class Course
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int CourseID { get; set; }
+		public string Title { get; set; }
+		public int Credits { get; set; }
+
+		public virtual ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
---
+++ Code/Samples/Samples.Semantics.Sample09.EntityFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Samples.Semantics.Sample09.EntityFramework")]
+[assembly: AssemblyDescription("Inventor.Semantics.EF")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andrei Bychko")]
+[assembly: AssemblyProduct("Inventor")]
+[assembly: AssemblyCopyright("Copyright © Andrei Bychko 2014-2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fa3dd113-2130-40d0-bbb4-68b9ef03d3a2")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
