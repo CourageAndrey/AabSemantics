36774db310bbd04f8c17c4de9668069ca8af66cc
Andrei Bychko <andrei.a.bychko@gmail.com>
2023-03-16 12:39:06 +0200

    Implement simple WebAPI (REST) client.
    Warning: some of links do not work properly!

---
+++ Code/Clients/Inventor.SimpleRestClient/Controllers/SemanticNetworkController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Inventor.Semantics.Xml;
+
+namespace Inventor.SimpleRestClient.Controllers
+{
+	[ApiController, Route("[controller]")]
+	public class SemanticNetworkController : ControllerBase
+	{
+		private readonly ILogger<SemanticNetworkController> _logger;
+		private readonly IDataService _dataService;
+
+		public SemanticNetworkController(ILogger<SemanticNetworkController> logger, IDataService dataService)
+		{
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			_dataService = dataService ?? throw new ArgumentNullException(nameof(dataService));
+		}
+
+		[HttpGet(Name = "GetSemanticNetwork")]
+		public SemanticNetwork Get()
+		{
+			var semanticNetwork = _dataService.GetSemanticNetwork();
+
+			return new SemanticNetwork(semanticNetwork);
+		}
+	}
+}
