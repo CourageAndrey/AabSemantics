50ab8848e22c698512a00d617bf45fbcccb7e6d6
Andrei Bychko <courage.andrey@gmail.com>
2023-03-23 16:46:51 +0200

    Finish implementation of JSON [de]serializaing mechanism (the same as XML one).

--- Code/Core/Inventor.Semantics/Utils/JsonHelper.cs
+++ Code/Core/Inventor.Semantics/Utils/JsonHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Json;
 using System.Text;
@@ -7,25 +8,91 @@ namespace Inventor.Semantics.Utils
 {
 	public static class JsonHelper
 	{
-		public static String SerializeToJsonString(this DataContractJsonSerializer serializer, Object snapshot)
+		public static readonly Encoding Encoding = Encoding.UTF8;
+
+		#region Serializers cache
+
+		private static readonly Dictionary<Type, DataContractJsonSerializer> _serializers = new Dictionary<Type, DataContractJsonSerializer>();
+		private static readonly Object _serializersLock = new Object();
+
+		public static DataContractJsonSerializer AcquireJsonSerializer<T>()
+		{
+			return AcquireJsonSerializer(typeof(T));
+		}
+
+		public static DataContractJsonSerializer AcquireJsonSerializer(this Type type)
+		{
+			lock (_serializersLock)
+			{
+				DataContractJsonSerializer serializer;
+				if (!_serializers.TryGetValue(type, out serializer))
+				{
+					serializer = _serializers[type] = new DataContractJsonSerializer(type);
+				}
+				return serializer;
+			}
+		}
+
+		public static void DefineCustomJsonSerializer(this Type type, DataContractJsonSerializer serializer)
+		{
+			lock (_serializersLock)
+			{
+				_serializers[type] = serializer;
+			}
+		}
+
+		public static void DefineCustomJsonSerializer<T>(this DataContractJsonSerializer serializer)
+		{
+			typeof(T).DefineCustomJsonSerializer(serializer);
+		}
+
+		#endregion
+
+		#region Serialization
+
+		public static String SerializeToJsonString(this Object entity)
 		{
 			Byte[] jsonBytes;
 			using (var memoryStream = new MemoryStream())
 			{
-				serializer.WriteObject(memoryStream, snapshot);
+				AcquireJsonSerializer(entity.GetType()).WriteObject(memoryStream, entity);
 				jsonBytes = memoryStream.ToArray();
 			}
-			return Encoding.UTF8.GetString(jsonBytes, 0, jsonBytes.Length);
+			return Encoding.GetString(jsonBytes, 0, jsonBytes.Length);
 		}
 
-		public static Object DeserializeFromJsonString(this DataContractJsonSerializer serializer, String json)
+		public static void SerializeToJsonFile(this Object entity, String fileName)
 		{
-			Byte[] jsonBytes = Encoding.UTF8.GetBytes(json);
-			using (var memoryStream = new MemoryStream(jsonBytes))
+			File.WriteAllText(fileName, entity.SerializeToJsonString());
+		}
+
+		#endregion
+
+		#region Deserialization
+
+		public static T DeserializeFromJsonStream<T>(this Stream stream)
+		{
+			return (T) AcquireJsonSerializer<T>().ReadObject(stream);
+		}
+
+		public static T DeserializeFromJsonBytes<T>(this Byte[] bytes)
+		{
+			using (var memoryStream = new MemoryStream(bytes))
 			{
-				memoryStream.Seek(0, SeekOrigin.Begin);
-				return serializer.ReadObject(memoryStream);
+				return memoryStream.DeserializeFromJsonStream<T>();
 			}
 		}
+
+		public static T DeserializeFromJsonFile<T>(this String file)
+		{
+			return File.ReadAllBytes(file).DeserializeFromJsonBytes<T>();
+		}
+
+		public static T DeserializeFromJsonText<T>(this String json)
+		{
+			return Encoding.GetBytes(json).DeserializeFromJsonBytes<T>();
+		}
+
+		#endregion
 	}
 }
