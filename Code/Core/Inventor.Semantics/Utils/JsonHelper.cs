2cb42822ff0965a2a886cfd7e5ab4efcd76c062d
Andrei Bychko <courage.andrey@gmail.com>
2023-03-23 15:24:49 +0200

    Extract JsonHelper class, which simplifies serialization using DataContract attributes.

---
+++ Code/Core/Inventor.Semantics/Utils/JsonHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Inventor.Semantics.Utils
+{
+	public static class JsonHelper
+	{
+		public static String SerializeToJsonString(this DataContractJsonSerializer serializer, Object snapshot)
+		{
+			Byte[] jsonBytes;
+			using (var memoryStream = new MemoryStream())
+			{
+				serializer.WriteObject(memoryStream, snapshot);
+				jsonBytes = memoryStream.ToArray();
+			}
+			return Encoding.UTF8.GetString(jsonBytes, 0, jsonBytes.Length);
+		}
+
+		public static Object DeserializeFromJsonString(this DataContractJsonSerializer serializer, String json)
+		{
+			Byte[] jsonBytes = Encoding.UTF8.GetBytes(json);
+			using (var memoryStream = new MemoryStream(jsonBytes))
+			{
+				memoryStream.Seek(0, SeekOrigin.Begin);
+				return serializer.ReadObject(memoryStream);
+			}
+		}
+	}
+}
