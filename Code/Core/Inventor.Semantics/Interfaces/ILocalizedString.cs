5b66d573925c8d325a2e65cd60c9dadd9ad12574
Andrei Bychko <courage.andrey@gmail.com>
2023-03-24 12:11:07 +0200

    Get rid of two dangerous typecasts in time of LocalizedString serialization.

--- Code/Core/Inventor.Semantics/Interfaces/ILocalizedString.cs
+++ Code/Core/Inventor.Semantics/Interfaces/ILocalizedString.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Inventor.Semantics.Localization;
 
 namespace Inventor.Semantics
 {
 	public interface ILocalizedString
 	{
 		String GetValue(ILanguage language);
+	}
+
+	public static class LocalizedStringExtensions
+	{
+		public static Dictionary<String, String> AsDictionary(this ILocalizedString localizedString)
+		{
+			if (localizedString == null)
+			{
+				throw new ArgumentNullException(nameof(localizedString));
+			}
+			else if (localizedString is LocalizedStringVariable variable)
+			{
+				return variable.Locales.ToDictionary(
+					locale => locale,
+					locale => variable.GetValue(locale));
+			}
+			else if (localizedString is LocalizedStringConstant constant)
+			{
+				var language = Language.Default;
+				return new Dictionary<String, String>
+				{
+					{ language.Culture, constant.GetValue(language) },
+				};
+			}
+			else
+			{
+				throw new NotSupportedException(localizedString.GetType().FullName);
+			}
+		}
 	}
 }
