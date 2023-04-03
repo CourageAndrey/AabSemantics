82b80822e4604f1145b62164378debe2f47a12ec
Andrei Bychko <courage.andrey@gmail.com>
2023-04-03 08:47:59 +0200

    Cover metadata Repositories with additional unit tests.

--- Code/Tests/Inventor.Semantics.Test/Metadata/RepositoriesTest.cs
+++ Code/Tests/Inventor.Semantics.Test/Metadata/RepositoriesTest.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using NUnit.Framework;
 
 using Inventor.Semantics.Metadata;
+using Inventor.Semantics.Modules.Boolean.Attributes;
+using Inventor.Semantics.Modules.Classification.Statements;
+using Inventor.Semantics.Answers;
+using Inventor.Semantics.Modules.Boolean.Questions;
 
 namespace Inventor.Semantics.Test.Metadata
 {
@@ -40,6 +44,46 @@ namespace Inventor.Semantics.Test.Metadata
 			Assert.Throws<ArgumentNullException>(() => Repositories.Questions = null);
 
 			Assert.DoesNotThrow(() => Repositories.Questions = new Repository<QuestionDefinition>());
+		}
+
+		[Test]
+		public void ImpossibleToSetNullAnswers()
+		{
+			Assert.Throws<ArgumentNullException>(() => Repositories.Answers = null);
+
+			Assert.DoesNotThrow(() => Repositories.Answers = new Repository<AnswerDefinition>());
+		}
+
+		[Test]
+		public void TypedAnduntypedRegistrationWorkTheSame()
+		{
+			// arrange
+			Repositories.Attributes.Definitions.Clear();
+			Repositories.Statements.Definitions.Clear();
+			Repositories.Questions.Definitions.Clear();
+			Repositories.Answers.Definitions.Clear();
+
+			// act
+			var attributeDefinition = Repositories.RegisterAttribute(typeof(IsValueAttribute), IsValueAttribute.Value, l => string.Empty);
+			var statementDefinition = Repositories.RegisterStatement(typeof(IsStatement), l => string.Empty, (semanticNetwork, result) => { });
+			var questionDefinition = Repositories.RegisterQuestion(typeof(CheckStatementQuestion), l => string.Empty);
+			var answerDefinition = Repositories.RegisterAnswer(typeof(BooleanAnswer));
+
+			Repositories.Attributes.Definitions.Clear();
+			Repositories.Statements.Definitions.Clear();
+			Repositories.Questions.Definitions.Clear();
+			Repositories.Answers.Definitions.Clear();
+
+			var attributeDefinitionT = Repositories.RegisterAttribute<IsValueAttribute>(IsValueAttribute.Value, l => string.Empty);
+			var statementDefinitionT = Repositories.RegisterStatement<IsStatement>(l => string.Empty, (statements, result, semanticNetwork) => { });
+			var questionDefinitionT = Repositories.RegisterQuestion<CheckStatementQuestion>(l => string.Empty);
+			var answerDefinitionT = Repositories.RegisterAnswer<BooleanAnswer>();
+
+			// assert
+			Assert.AreSame(attributeDefinition.Type, attributeDefinitionT.Type);
+			Assert.AreSame(statementDefinition.Type, statementDefinitionT.Type);
+			Assert.AreSame(questionDefinitionT.Type, questionDefinition.Type);
+			Assert.AreSame(answerDefinition.Type, answerDefinitionT.Type);
 		}
 	}
 }
