6afdcb0001f1f73ec4a1b420f08e1de94cc8cc06
Andrei Bychko <courage.andrey@gmail.com>
2023-06-30 18:08:03 +0200

    Split Questions/QuestionBuilderTest into module-related parts.

--- Code/Tests/AabSemantics.Tests/Questions/QuestionBuilderTest.cs
+++ Code/Tests/AabSemantics.Tests/Questions/QuestionBuilderTest.cs
@@ -6,17 +6,12 @@ using NUnit.Framework;
 using AabSemantics.Answers;
 using AabSemantics.Concepts;
 using AabSemantics.Localization;
-using AabSemantics.Modules.Boolean.Attributes;
 using AabSemantics.Modules.Boolean.Questions;
 using AabSemantics.Modules.Classification.Questions;
+using AabSemantics.Modules.Classification.Statements;
 using AabSemantics.Modules.Mathematics.Concepts;
 using AabSemantics.Modules.Mathematics.Questions;
-using AabSemantics.Modules.Mathematics.Statements;
 using AabSemantics.Modules.Mathematics.Tests;
-using AabSemantics.Modules.Processes.Attributes;
-using AabSemantics.Modules.Processes.Questions;
-using AabSemantics.Modules.Processes.Statements;
-using AabSemantics.Modules.Set.Questions;
 using AabSemantics.Modules.Set.Tests;
 using AabSemantics.Questions;
 using AabSemantics.Statements;
@@ -43,42 +38,6 @@ namespace AabSemantics.Tests.Questions
 
 			// assert
 			Assert.AreEqual(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenHowCompared_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateMathematicsTestData();
-
-			// act
-			var questionRegular = new ComparisonQuestion(semanticNetwork.Number0, semanticNetwork.Number2);
-			var answerRegular = (StatementAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (StatementAnswer) semanticNetwork.SemanticNetwork.Ask().HowCompared(semanticNetwork.Number0, semanticNetwork.Number2);
-
-			// assert
-			Assert.AreEqual(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenWhichConceptsBelongToSubjectArea_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new DescribeSubjectAreaQuestion(semanticNetwork.SubjectArea_Transport);
-			var answerRegular = (ConceptsAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (ConceptsAnswer) semanticNetwork.SemanticNetwork.Ask().WhichConceptsBelongToSubjectArea(semanticNetwork.SubjectArea_Transport);
-
-			// assert
-			Assert.IsTrue(answerRegular.Result.SequenceEqual(answerBuilder.Result));
 			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
 		}
 
@@ -119,132 +78,6 @@ namespace AabSemantics.Tests.Questions
 		}
 
 		[Test]
-		public void GivenWhichContainersInclude_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new EnumerateContainersQuestion(semanticNetwork.Part_Engine);
-			var answerRegular = (ConceptsAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (ConceptsAnswer) semanticNetwork.SemanticNetwork.Ask().WhichContainersInclude(semanticNetwork.Part_Engine);
-
-			// assert
-			Assert.IsTrue(answerRegular.Result.SequenceEqual(answerBuilder.Result));
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenWhichPartsHas_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new EnumeratePartsQuestion(semanticNetwork.Vehicle_Car);
-			var answerRegular = (ConceptsAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (ConceptsAnswer) semanticNetwork.SemanticNetwork.Ask().WhichPartsHas(semanticNetwork.Vehicle_Car);
-
-			// assert
-			Assert.IsTrue(answerRegular.Result.SequenceEqual(answerBuilder.Result));
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenWhichSignsHas_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new EnumerateSignsQuestion(semanticNetwork.Vehicle_Car, true);
-			var answerRegular = (ConceptsAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (ConceptsAnswer) semanticNetwork.SemanticNetwork.Ask().WhichSignsHas(semanticNetwork.Vehicle_Car);
-
-			// assert
-			Assert.IsTrue(answerRegular.Result.SequenceEqual(answerBuilder.Result));
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenToWhichSubjectAreasBelongs_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new FindSubjectAreaQuestion(semanticNetwork.Vehicle_Car);
-			var answerRegular = (ConceptsAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (ConceptsAnswer) semanticNetwork.SemanticNetwork.Ask().ToWhichSubjectAreasBelongs(semanticNetwork.Vehicle_Car);
-
-			// assert
-			Assert.IsTrue(answerRegular.Result.SequenceEqual(answerBuilder.Result));
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenIfHasSign_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new HasSignQuestion(semanticNetwork.Vehicle_Car, semanticNetwork.Sign_MotorType, true);
-			var answerRegular = (BooleanAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (BooleanAnswer) semanticNetwork.SemanticNetwork.Ask().IfHasSign(semanticNetwork.Vehicle_Car, semanticNetwork.Sign_MotorType);
-
-			// assert
-			Assert.AreEqual(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenIfHasSigns_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new HasSignsQuestion(semanticNetwork.Vehicle_Car, true);
-			var answerRegular = (BooleanAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (BooleanAnswer) semanticNetwork.SemanticNetwork.Ask().IfHasSigns(semanticNetwork.Vehicle_Car);
-
-			// assert
-			Assert.AreEqual(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenIfIsPartOf_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new IsPartOfQuestion(semanticNetwork.Part_Engine, semanticNetwork.Vehicle_Car);
-			var answerRegular = (BooleanAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (BooleanAnswer) semanticNetwork.SemanticNetwork.Ask().IfIsPartOf(semanticNetwork.Part_Engine, semanticNetwork.Vehicle_Car);
-
-			// assert
-			Assert.AreEqual(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
 		public void GivenIfIs_WhenBeingAsked_ThenBuildAndAskQuestion()
 		{
 			// arrange
@@ -263,144 +96,45 @@ namespace AabSemantics.Tests.Questions
 		}
 
 		[Test]
-		public void GivenIfIsSign_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new IsSignQuestion(semanticNetwork.Sign_AreaType);
-			var answerRegular = (BooleanAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (BooleanAnswer) semanticNetwork.SemanticNetwork.Ask().IfIsSign(semanticNetwork.Sign_AreaType);
-
-			// assert
-			Assert.AreEqual(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenIfConceptBelongsToSubjectArea_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new IsSubjectAreaQuestion(semanticNetwork.Vehicle_Car, semanticNetwork.SubjectArea_Transport);
-			var answerRegular = (BooleanAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (BooleanAnswer) semanticNetwork.SemanticNetwork.Ask().IfConceptBelongsToSubjectArea(semanticNetwork.Vehicle_Car, semanticNetwork.SubjectArea_Transport);
-
-			// assert
-			Assert.AreEqual(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenIfIsValue_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new IsValueQuestion(semanticNetwork.AreaType_Air);
-			var answerRegular = (BooleanAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (BooleanAnswer) semanticNetwork.SemanticNetwork.Ask().IfIsValue(semanticNetwork.AreaType_Air);
-
-			// assert
-			Assert.AreEqual(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenWhatIsMutualSequenceOfProcesses_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			var processA = ConceptCreationHelper.CreateConcept();
-			processA.WithAttribute(IsProcessAttribute.Value);
-			var processB = ConceptCreationHelper.CreateConcept();
-			processB.WithAttribute(IsProcessAttribute.Value);
-			semanticNetwork.SemanticNetwork.DeclareThat(processA).StartsBeforeOtherStarted(processB);
-			semanticNetwork.SemanticNetwork.DeclareThat(processA).FinishesAfterOtherFinished(processB);
-
-			// act
-			var questionRegular = new ProcessesQuestion(processA, processB);
-			var answerRegular = (StatementsAnswer<ProcessesStatement>) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (StatementsAnswer<ProcessesStatement>) semanticNetwork.SemanticNetwork.Ask().WhatIsMutualSequenceOfProcesses(processA, processB);
-
-			// assert
-			Assert.IsTrue(answerRegular.Result.SequenceEqual(answerBuilder.Result));
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenWhatIsSignValue_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			// act
-			var questionRegular = new SignValueQuestion(semanticNetwork.Vehicle_Car, semanticNetwork.Sign_MotorType);
-			var answerRegular = (ConceptAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = (ConceptAnswer) semanticNetwork.SemanticNetwork.Ask().WhatIsSignValue(semanticNetwork.Vehicle_Car, semanticNetwork.Sign_MotorType);
-
-			// assert
-			Assert.AreSame(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
-		public void GivenWhatIs_WhenBeingAsked_ThenBuildAndAskQuestion()
-		{
-			// arrange
-			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateSetTestData();
-
-			var render = TextRenders.PlainString;
-
-			// act
-			var questionRegular = new WhatQuestion(semanticNetwork.Vehicle_Car);
-			var answerRegular = questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
-
-			var answerBuilder = semanticNetwork.SemanticNetwork.Ask().WhatIs(semanticNetwork.Vehicle_Car);
-
-			// assert
-			Assert.AreEqual(render.RenderText(answerRegular.Description, language).ToString(), render.RenderText(answerBuilder.Description, language).ToString());
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
-		}
-
-		[Test]
 		public void GivenPreconditions_WhenBeingAsked_ThenTakeThemIntoAccount()
 		{
 			// arrange
 			var language = Language.Default;
-			var semanticNetwork = new SemanticNetwork(language).CreateMathematicsTestData();
+			var semanticNetwork = new SemanticNetwork(language);
 
-			var numberMinus1 = ConceptCreationHelper.CreateConcept();
-			numberMinus1.WithAttribute(IsValueAttribute.Value);
+			IConcept ancestor, middle, descendant;
+			semanticNetwork.Concepts.Add(ancestor = "ancestor".CreateConcept());
+			semanticNetwork.Concepts.Add(middle = "middle".CreateConcept());
+			semanticNetwork.Concepts.Add(descendant = "descendant".CreateConcept());
 
-			var precondition = new ComparisonStatement(null, numberMinus1, semanticNetwork.Number0, ComparisonSigns.IsLessThan);
+			var existingStatement = semanticNetwork.DeclareThat(ancestor).IsAncestorOf(middle);
+			var precondition = new IsStatement(null, middle, descendant);
 			var preconditions = new IStatement[] { precondition };
 
 			// act
-			var questionRegular = new ComparisonQuestion(numberMinus1, semanticNetwork.Number1, preconditions);
-			var answerRegular = (StatementAnswer) questionRegular.Ask(semanticNetwork.SemanticNetwork.Context);
+			var questionRegularWithoutPreconditions = new IsQuestion(descendant, ancestor);
+			var answerRegularWithoutPreconditions = (BooleanAnswer) questionRegularWithoutPreconditions.Ask(semanticNetwork.Context);
 
-			var answerBuilder = (StatementAnswer) semanticNetwork.SemanticNetwork.Supposing(preconditions).Ask().HowCompared(numberMinus1, semanticNetwork.Number1);
+			var answerBuilderWithoutPreconditions = (BooleanAnswer) semanticNetwork.Ask().IfIs(descendant, ancestor);
+
+			var questionRegularWithPreconditions = new IsQuestion(descendant, ancestor, preconditions);
+			var answerRegularWithPreconditions = (BooleanAnswer) questionRegularWithPreconditions.Ask(semanticNetwork.Context);
+
+			var answerBuilderWithPreconditions = (BooleanAnswer) semanticNetwork.Supposing(preconditions).Ask().IfIs(descendant, ancestor);
 
 			// assert
-			Assert.AreEqual(answerRegular.Result, answerBuilder.Result);
-			Assert.IsTrue(answerRegular.Explanation.Statements.SequenceEqual(answerBuilder.Explanation.Statements));
+			Assert.IsFalse(answerRegularWithoutPreconditions.Result);
+			Assert.AreEqual(0, answerRegularWithoutPreconditions.Explanation.Statements.Count);
+			Assert.IsFalse(answerBuilderWithoutPreconditions.Result);
+			Assert.AreEqual(0, answerBuilderWithoutPreconditions.Explanation.Statements.Count);
+			Assert.IsTrue(answerRegularWithPreconditions.Result);
+			Assert.AreEqual(2, answerRegularWithPreconditions.Explanation.Statements.Count);
+			Assert.IsTrue(answerRegularWithPreconditions.Result);
+			Assert.IsTrue(answerRegularWithPreconditions.Explanation.Statements.Contains(existingStatement));
+			Assert.IsTrue(answerRegularWithPreconditions.Explanation.Statements.Contains(precondition));
+			Assert.AreEqual(2, answerBuilderWithPreconditions.Explanation.Statements.Count);
+			Assert.IsTrue(answerBuilderWithPreconditions.Explanation.Statements.Contains(existingStatement));
+			Assert.IsTrue(answerBuilderWithPreconditions.Explanation.Statements.Contains(precondition));
 		}
 
 		[Test]
