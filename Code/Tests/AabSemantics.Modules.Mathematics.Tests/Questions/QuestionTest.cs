85a9274789ec844014a532f98b550c667478a2ee
Andrei Bychko <courage.andrey@gmail.com>
2023-06-30 18:08:47 +0200

    Split Questions/QuestionTest into module-related parts.

---
+++ Code/Tests/AabSemantics.Modules.Mathematics.Tests/Questions/QuestionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using AabSemantics.Concepts;
+using AabSemantics.Modules.Mathematics.Questions;
+
+namespace AabSemantics.Modules.Mathematics.Tests.Questions
+{
+	[TestFixture]
+	public class QuestionTest
+	{
+		[Test]
+		[TestCaseSource(nameof(CreateQuestionsArgumentNullException))]
+		public void GivenNullArguments_WhenCreateQuestions_ThenFail(Func<IQuestion> constructor)
+		{
+			Assert.Throws<ArgumentNullException>(() => constructor());
+		}
+
+		private static IEnumerable<object[]> CreateQuestionsArgumentNullException()
+		{
+			IConcept concept = "test".CreateConcept();
+
+			yield return new object[] { new Func<IQuestion>(() => new ComparisonQuestion(null, concept)) };
+			yield return new object[] { new Func<IQuestion>(() => new ComparisonQuestion(concept, null)) };
+		}
+	}
+}
--- Code/Tests/AabSemantics.Tests/Questions/QuestionTest.cs
+++ Code/Tests/AabSemantics.Tests/Questions/QuestionTest.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 using NUnit.Framework;
 
 using AabSemantics.Answers;
 using AabSemantics.Concepts;
 using AabSemantics.Localization;
-using AabSemantics.Modules.Boolean.Attributes;
 using AabSemantics.Modules.Boolean.Questions;
 using AabSemantics.Modules.Classification.Questions;
-using AabSemantics.Modules.Mathematics.Concepts;
-using AabSemantics.Modules.Mathematics.Questions;
-using AabSemantics.Modules.Mathematics.Statements;
-using AabSemantics.Modules.Processes.Questions;
-using AabSemantics.Modules.Set.Questions;
+using AabSemantics.Modules.Classification.Statements;
 using AabSemantics.Questions;
 using AabSemantics.Statements;
 
@@ -30,39 +24,30 @@ namespace AabSemantics.Tests.Questions
 			var language = Language.Default;
 			var semanticNetwork = new SemanticNetwork(language);
 
-			var concept1 = ConceptCreationHelper.CreateConcept();
-			concept1.WithAttribute(IsValueAttribute.Value);
-			var concept2 = ConceptCreationHelper.CreateConcept();
-			concept2.WithAttribute(IsValueAttribute.Value);
-			var concept3 = ConceptCreationHelper.CreateConcept();
-			concept3.WithAttribute(IsValueAttribute.Value);
-			semanticNetwork.Concepts.Add(concept1);
-			semanticNetwork.Concepts.Add(concept2);
-			semanticNetwork.Concepts.Add(concept3);
+			IConcept parentConcept, middleConcept, childConcept;
+			semanticNetwork.Concepts.Add(parentConcept = ConceptCreationHelper.CreateConcept());
+			semanticNetwork.Concepts.Add(middleConcept = ConceptCreationHelper.CreateConcept());
+			semanticNetwork.Concepts.Add(childConcept = ConceptCreationHelper.CreateConcept());
 
-			var initialComparison = semanticNetwork.DeclareThat(concept1).IsEqualTo(concept2);
+			var initialStatement = semanticNetwork.DeclareThat(parentConcept).IsAncestorOf(middleConcept);
 
-			var preconditionComparison = new ComparisonStatement(null, concept2, concept3, ComparisonSigns.IsEqualTo);
+			var preconditionStatement = new IsStatement(null, middleConcept, childConcept);
 			// ... and do not add it to semantic network
 
 			// act
-			var answerWithoutPreconditions = semanticNetwork.Ask().HowCompared(concept1, concept3);
-			var answerWithPreconditions = semanticNetwork.Supposing(new IStatement[] { preconditionComparison }).Ask().HowCompared(concept1, concept3);
+			var answerWithoutPreconditions = (BooleanAnswer) semanticNetwork.Ask().IfIs(childConcept, parentConcept);
+			var answerWithPreconditions = (BooleanAnswer) semanticNetwork.Supposing(new IStatement[] { preconditionStatement }).Ask().IfIs(childConcept, parentConcept);
 
 			// assert
-			Assert.IsTrue(answerWithoutPreconditions.IsEmpty);
+			Assert.IsFalse(answerWithoutPreconditions.IsEmpty);
+			Assert.IsFalse(answerWithoutPreconditions.Result);
 			Assert.AreEqual(0, answerWithoutPreconditions.Explanation.Statements.Count);
 
 			Assert.IsFalse(answerWithPreconditions.IsEmpty);
-			var resultComparison = (ComparisonStatement) ((StatementAnswer) answerWithPreconditions).Result;
-			Assert.AreSame(concept1, resultComparison.LeftValue);
-			Assert.AreSame(concept3, resultComparison.RightValue);
-			Assert.AreSame(ComparisonSigns.IsEqualTo, resultComparison.ComparisonSign);
+			Assert.IsTrue(answerWithPreconditions.Result);
 			Assert.AreEqual(2, answerWithPreconditions.Explanation.Statements.Count);
-			Assert.IsTrue(answerWithPreconditions.Explanation.Statements.Contains(initialComparison));
-			Assert.IsTrue(answerWithPreconditions.Explanation.Statements.Contains(preconditionComparison));
-
-			Assert.AreSame(initialComparison, semanticNetwork.Statements.Single());
+			Assert.IsTrue(answerWithPreconditions.Explanation.Statements.Contains(initialStatement));
+			Assert.IsTrue(answerWithPreconditions.Explanation.Statements.Contains(preconditionStatement));
 		}
 
 		[Test]
@@ -77,53 +62,29 @@ namespace AabSemantics.Tests.Questions
 			IConcept concept = "test".CreateConcept();
 
 			yield return new object[] { new Func<IQuestion>(() => new CheckStatementQuestion(null)) };
-			yield return new object[] { new Func<IQuestion>(() => new ComparisonQuestion(null, concept)) };
-			yield return new object[] { new Func<IQuestion>(() => new ComparisonQuestion(concept, null)) };
-			yield return new object[] { new Func<IQuestion>(() => new DescribeSubjectAreaQuestion(null)) };
 			yield return new object[] { new Func<IQuestion>(() => new EnumerateAncestorsQuestion(null)) };
-			yield return new object[] { new Func<IQuestion>(() => new EnumerateContainersQuestion(null)) };
 			yield return new object[] { new Func<IQuestion>(() => new EnumerateDescendantsQuestion(null)) };
-			yield return new object[] { new Func<IQuestion>(() => new EnumeratePartsQuestion(null)) };
-			yield return new object[] { new Func<IQuestion>(() => new EnumerateSignsQuestion(null, false)) };
-			yield return new object[] { new Func<IQuestion>(() => new FindSubjectAreaQuestion(null)) };
-			yield return new object[] { new Func<IQuestion>(() => new GetCommonQuestion(null, concept)) };
-			yield return new object[] { new Func<IQuestion>(() => new GetCommonQuestion(concept, null)) };
-			yield return new object[] { new Func<IQuestion>(() => new GetDifferencesQuestion(null, concept)) };
-			yield return new object[] { new Func<IQuestion>(() => new GetDifferencesQuestion(concept, null)) };
-			yield return new object[] { new Func<IQuestion>(() => new HasSignQuestion(null, concept, false)) };
-			yield return new object[] { new Func<IQuestion>(() => new HasSignQuestion(concept, null, false)) };
-			yield return new object[] { new Func<IQuestion>(() => new HasSignsQuestion(null, false)) };
-			yield return new object[] { new Func<IQuestion>(() => new IsPartOfQuestion(null, concept)) };
-			yield return new object[] { new Func<IQuestion>(() => new IsPartOfQuestion(concept, null)) };
 			yield return new object[] { new Func<IQuestion>(() => new IsQuestion(null, concept)) };
 			yield return new object[] { new Func<IQuestion>(() => new IsQuestion(concept, null)) };
-			yield return new object[] { new Func<IQuestion>(() => new IsSignQuestion(null)) };
-			yield return new object[] { new Func<IQuestion>(() => new IsSubjectAreaQuestion(null, concept)) };
-			yield return new object[] { new Func<IQuestion>(() => new IsSubjectAreaQuestion(concept, null)) };
-			yield return new object[] { new Func<IQuestion>(() => new IsValueQuestion(null)) };
-			yield return new object[] { new Func<IQuestion>(() => new ProcessesQuestion(null, concept)) };
-			yield return new object[] { new Func<IQuestion>(() => new ProcessesQuestion(concept, null)) };
-			yield return new object[] { new Func<IQuestion>(() => new SignValueQuestion(null, concept)) };
-			yield return new object[] { new Func<IQuestion>(() => new SignValueQuestion(concept, null)) };
-			yield return new object[] { new Func<IQuestion>(() => new WhatQuestion(null)) };
 		}
 
 		[Test]
 		public void GivenAllParameterSet_WhenCreateChildAnswer_ThenSucceed()
 		{
 			// arrange
-			var concept = "test".CreateConcept();
-			var question = new HasSignsQuestion(concept, false);
+			var childConcept = "child".CreateConcept();
+			var parentConcept = "parent".CreateConcept();
+			var question = new IsQuestion(childConcept, parentConcept);
 			var answer = Answer.CreateUnknown();
 			var transitives = Array.Empty<IStatement>();
 
 			// act
-			var child = new ChildAnswer(question, answer, transitives);
+			var childAnswer = new ChildAnswer(question, answer, transitives);
 
 			// assert
-			Assert.AreSame(question, child.Question);
-			Assert.AreSame(answer, child.Answer);
-			Assert.AreSame(transitives, child.TransitiveStatements);
+			Assert.AreSame(question, childAnswer.Question);
+			Assert.AreSame(answer, childAnswer.Answer);
+			Assert.AreSame(transitives, childAnswer.TransitiveStatements);
 		}
 	}
 }
