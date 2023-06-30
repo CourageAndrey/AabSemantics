bea09970b241159889b9c3d4f6b3e5eda5765a9a
Andrei Bychko <andrei.a.bychko@gmail.com>
2023-06-30 18:07:13 +0200

    Split Concepts/AttributesExtensionTest into module-related parts.

--- Code/Tests/AabSemantics.Modules.Mathematics.Tests/Concepts/AttributesExtensionTest.cs
+++ Code/Tests/AabSemantics.Modules.Mathematics.Tests/Concepts/AttributesExtensionTest.cs
@@ -2,8 +2,9 @@
 
 using AabSemantics.Concepts;
 using AabSemantics.Modules.Boolean.Attributes;
+using AabSemantics.Modules.Mathematics.Attributes;
 
-namespace AabSemantics.Tests.Concepts
+namespace AabSemantics.Modules.Mathematics.Tests.Concepts
 {
 	[TestFixture]
 	public class AttributesExtensionTest
@@ -16,16 +17,25 @@ namespace AabSemantics.Tests.Concepts
 			// 0. no attributes added
 			Assert.AreEqual(0, concept.Attributes.Count);
 			Assert.IsFalse(concept.HasAttribute<IsValueAttribute>());
+			Assert.IsFalse(concept.HasAttribute<IsComparisonSignAttribute>());
 
 			// 1. add IsValueAttribute
 			concept.WithAttribute(IsValueAttribute.Value);
 			Assert.AreEqual(1, concept.Attributes.Count);
 			Assert.IsTrue(concept.HasAttribute<IsValueAttribute>());
+			Assert.IsFalse(concept.HasAttribute<IsComparisonSignAttribute>());
 
-			// 2. remove all attributes
+			// 2. add IsComparisonSignAttribute
+			concept.WithAttribute(IsComparisonSignAttribute.Value);
+			Assert.AreEqual(2, concept.Attributes.Count);
+			Assert.IsTrue(concept.HasAttribute<IsValueAttribute>());
+			Assert.IsTrue(concept.HasAttribute<IsComparisonSignAttribute>());
+
+			// 3. remove all attributes
 			concept.WithoutAttributes();
 			Assert.AreEqual(0, concept.Attributes.Count);
 			Assert.IsFalse(concept.HasAttribute<IsValueAttribute>());
+			Assert.IsFalse(concept.HasAttribute<IsComparisonSignAttribute>());
 		}
 	}
 }
--- Code/Tests/AabSemantics.Modules.Processes.Tests/Concepts/AttributesExtensionTest.cs
+++ Code/Tests/AabSemantics.Modules.Processes.Tests/Concepts/AttributesExtensionTest.cs
@@ -2,8 +2,9 @@
 
 using AabSemantics.Concepts;
 using AabSemantics.Modules.Boolean.Attributes;
+using AabSemantics.Modules.Processes.Attributes;
 
-namespace AabSemantics.Tests.Concepts
+namespace AabSemantics.Modules.Processes.Tests.Concepts
 {
 	[TestFixture]
 	public class AttributesExtensionTest
@@ -16,16 +17,25 @@ namespace AabSemantics.Tests.Concepts
 			// 0. no attributes added
 			Assert.AreEqual(0, concept.Attributes.Count);
 			Assert.IsFalse(concept.HasAttribute<IsValueAttribute>());
+			Assert.IsFalse(concept.HasAttribute<IsSequenceSignAttribute>());
 
 			// 1. add IsValueAttribute
 			concept.WithAttribute(IsValueAttribute.Value);
 			Assert.AreEqual(1, concept.Attributes.Count);
 			Assert.IsTrue(concept.HasAttribute<IsValueAttribute>());
+			Assert.IsFalse(concept.HasAttribute<IsSequenceSignAttribute>());
 
-			// 2. remove all attributes
+			// 2. add IsSequenceSignAttribute
+			concept.WithAttribute(IsSequenceSignAttribute.Value);
+			Assert.AreEqual(2, concept.Attributes.Count);
+			Assert.IsTrue(concept.HasAttribute<IsValueAttribute>());
+			Assert.IsTrue(concept.HasAttribute<IsSequenceSignAttribute>());
+
+			// 3. remove all attributes
 			concept.WithoutAttributes();
 			Assert.AreEqual(0, concept.Attributes.Count);
 			Assert.IsFalse(concept.HasAttribute<IsValueAttribute>());
+			Assert.IsFalse(concept.HasAttribute<IsSequenceSignAttribute>());
 		}
 	}
 }
