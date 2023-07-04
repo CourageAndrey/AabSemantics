14cd288395baccb694410f2a1a498eee4a78f6e4
Andrei Bychko <andrei.a.bychko@gmail.com>
2023-07-04 17:12:53 +0200

    Finish statements .Equals() method covering with unit tests.

---
+++ Code/Tests/AabSemantics.Modules.Processes.Tests/Statements/ProcessesStatementTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+using AabSemantics.Concepts;
+using AabSemantics.Modules.Processes.Attributes;
+using AabSemantics.Modules.Processes.Concepts;
+using AabSemantics.Modules.Processes.Statements;
+
+namespace AabSemantics.Modules.Processes.Tests.Statements
+{
+	[TestFixture]
+	public class ProcessesStatementTest
+	{
+		[Test]
+		public void GivenNullOther_WhenEquals_ThenReturnFalse()
+		{
+			// arrange
+			IConcept	process1 = 1.CreateConcept().WithAttribute(IsProcessAttribute.Value),
+						process2 = 2.CreateConcept().WithAttribute(IsProcessAttribute.Value);
+			var statement = new ProcessesStatement(null, process1, process2, SequenceSigns.Causes);
+
+			// act && assert
+			Assert.IsFalse(statement.Equals(null));
+		}
+	}
+}
---
+++ Code/Tests/AabSemantics.Modules.Set.Tests/Statements/HasPartStatementTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+using AabSemantics.Concepts;
+using AabSemantics.Modules.Set.Statements;
+
+namespace AabSemantics.Modules.Set.Tests.Statements
+{
+	[TestFixture]
+	public class HasPartStatementTest
+	{
+		[Test]
+		public void GivenNullOther_WhenEquals_ThenReturnFalse()
+		{
+			// arrange
+			IConcept	concept1 = 1.CreateConcept(),
+						concept2 = 2.CreateConcept();
+			var statement = new HasPartStatement(null, concept1, concept2);
+
+			// act && assert
+			Assert.IsFalse(statement.Equals(null));
+		}
+	}
+}
