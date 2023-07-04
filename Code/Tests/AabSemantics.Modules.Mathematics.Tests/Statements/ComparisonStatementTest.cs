ee31cd835ce1a48121d578d1fce525b08602e0db
Andrei Bychko <courage.andrey@gmail.com>
2023-07-04 17:12:53 +0200

    Finish statements .Equals() method covering with unit tests.

---
+++ Code/Tests/AabSemantics.Modules.Mathematics.Tests/Statements/ComparisonStatementTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+using AabSemantics.Concepts;
+using AabSemantics.Modules.Boolean.Attributes;
+using AabSemantics.Modules.Mathematics.Concepts;
+using AabSemantics.Modules.Mathematics.Statements;
+
+namespace AabSemantics.Modules.Mathematics.Tests.Statements
+{
+	[TestFixture]
+	public class ComparisonStatementTest
+	{
+		[Test]
+		public void GivenNullOther_WhenEquals_ThenReturnFalse()
+		{
+			// arrange
+			IConcept	left = 1.CreateConcept().WithAttribute(IsValueAttribute.Value),
+						right = 2.CreateConcept().WithAttribute(IsValueAttribute.Value);
+			var statement = new ComparisonStatement(null, left, right, ComparisonSigns.IsLessThan);
+
+			// act && assert
+			Assert.IsFalse(statement.Equals(null));
+		}
+	}
+}
---
+++ Code/Tests/AabSemantics.Modules.Set.Tests/Statements/HasSignStatementTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+using AabSemantics.Concepts;
+using AabSemantics.Modules.Set.Attributes;
+using AabSemantics.Modules.Set.Statements;
+
+namespace AabSemantics.Modules.Set.Tests.Statements
+{
+	[TestFixture]
+	public class HasSignStatementTest
+	{
+		[Test]
+		public void GivenNullOther_WhenEquals_ThenReturnFalse()
+		{
+			// arrange
+			IConcept	concept = 1.CreateConcept(),
+						sign = 2.CreateConcept().WithAttribute(IsSignAttribute.Value);
+			var statement = new HasSignStatement(null, concept, sign);
+
+			// act && assert
+			Assert.IsFalse(statement.Equals(null));
+		}
+	}
+}
