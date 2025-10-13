a0d5e20f50439b9905b69785eafd1cf6a1173a40
Andrei Bychko <andrei.a.bychko@gmail.com>
2025-10-13 14:47:36 +0200

    Make it possible to test async exceptions correctly.

---
+++ Code/Tests/AabSemantics.TestCore/Async.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace AabSemantics.TestCore
+{
+	public static class Async
+	{
+		public static ExceptionT Throws<ExceptionT>(Action method)
+			where ExceptionT : Exception
+		{
+			var aggregateException = Assert.Throws<AggregateException>(() => method());
+			var trueException = (ExceptionT) aggregateException.InnerExceptions.Single();
+			Assert.That(trueException, Is.TypeOf<ExceptionT>());
+			return trueException;
+		}
+	}
+}
